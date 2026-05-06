fef917800b2800dcaedc4f3d55139808d4c6dac9
lea03 <ler002@edu.zealand.dk>
2026-05-06 13:13:30 +0200

    alt det gode + færdig forside til mobil

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,4 @@
-<?php
-/**
- * @var db $db
- */
 
-require "settings/init.php";
-?>
 <!DOCTYPE html>
 <html lang="da">
 <head>
@@ -22,6 +16,49 @@ require "settings/init.php";
 </head>
 
 <body>
+
+
+
+
+<nav class="navbar container d-flex bg-background">
+    <div class="flex-row justify-content-evenly align-items-center">
+
+        <div class="col-12">
+            <div class="col-6">
+        <a class="navbar-brand" href="#">
+            <img src="/images/logo.png" alt="Logo" height="40">
+        </a></div>
+
+        <div class="col-6">
+            <i class="fa-solid fa-bars fs-3"></i>
+        </div>
+    </div>
+    </div>
+</nav>
+
+
+<!-- Topbar med logo og menu-knap -->
+<div class="container-fluid m-0 bg-beige overflow-hidden">
+    <div class="row align-items-center justify-content-between py-2 px-2 py-md-3 px-md-3">
+        <!-- Venstre: Logo -->
+        <a href="/index.php" class="col-1 d-md-none d-flex align-items-center p-0">
+            <img src="/images/logo.png"  alt="logo" class="img-fluid">
+        </a>
+        <a href="/index.php" class="col-2 d-none d-md-flex align-items-center p-0">
+            <img src="" alt="logo" class="img-fluid">
+        </a>
+
+        <!-- Højre: Menu-knap -->
+        <div class="col-auto p-0 d-flex align-items-center">
+            <button id="openDropdown" class="impact bg-black rounded-pill text-white d-flex align-items-center justify-content-center border-0 py-1 px-3 px-lg-4 menu-header">
+                <i class="fa-solid fa-bars text-white ps-2"></i>
+            </button>
+        </div>
+    </div>
+</div>
+
+
+
 
 
 </body>
--- index.php
+++ index.php
@@ -21,9 +21,125 @@ require "settings/init.php";
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body>
+<body class="bg-background">
 
 <h1 class="allura">Det virker 🥳</h1>
+
+<div aria-hidden="true" >
+    <img src="waves-phone/navwave.png" class="waves position-relative w-100" alt="">
+</div>
+
+<div class="container-fluid p-0 d-md-none">
+    <img src="images/header-image.jpg" alt="header-image" class="img-fluid" style="margin-top: -70px">
+</div>
+
+<div class="container-fluid p-0 d-none d-md-block">
+    <img src="images/header-image-tablet.jpg" alt="header-image" class="img-fluid" style="margin-top: -115px">
+</div>
+
+<div aria-hidden="true" class="d-md-none">
+    <img src="waves-phone/green-wave.png" class="waves position-relative w-100" alt="" style="margin-top: -125px">
+</div>
+
+<div aria-hidden="true" class="d-none d-md-block">
+    <img src="waves-phone/green-wave.png" class="waves position-relative w-100" alt="" style="margin-top: -200px">
+</div>
+
+<div class="container-fluid bg-sage-green ps-4" style="margin-top: -20px">
+
+    <div class="row">
+        <div class="col-12 mt-3">
+            <div class="allura fs-5">Velkommen til Vedelsbo</div>
+        </div>
+        <div class="col-12">
+            <h1 class="cormorant pe-5 fw-bold">Hjælp til et godt og trygt liv</h1>
+        </div>
+        <div class="col-12">
+            <div class="inter header-text-inter pe-4 ">Et socialpsykiatrisk botilbud med en hjemlig atmosfære for voksne med en sindslidelse. </div>
+        </div>
+
+        <div class="mt-3">
+        <button class="bg-dark-brown rounded-pill border-0 header-button-text py-2 px-3">
+            Læs mere om Vedelsbo <i class="fa-solid fa-angle-right"></i>
+        </button>
+    </div>
+    </div>
+</div>
+
+<div aria-hidden="true">
+    <img src="waves-phone/green-wavywave.png" class="waves img-fluid  p-0 m-0" alt="" >
+</div>
+
+<div aria-hidden="true" class="text-end" style="margin-top: -40px">
+    <img src="images/Dekoration.png" class="  pe-4 m-0" alt="" style="width: 90px">
+</div>
+
+
+<div class="container mt-3 d-flex justify-content-center align-items-center">
+    <div class="row justify-content-center">
+
+        <div class="col-12 col-md-8 col-lg-6 text-center mt-3">
+
+            <img src="shapes/light-green-shape-heart.png" alt="light-green-shape" class="mb-3" style="width: 100px">
+
+            <strong class="cormorant d-block mb-2 front-page-shape-header">
+                HVAD VI STÅR FOR
+            </strong>
+
+            <p class="inter front-page-shape-text px-3">
+                en masse tekst om alt muligt spændene tihi. en masse tekst om alt muligt spændene tihi.
+            </p>
+
+        </div>
+
+        <div class="col-12 col-md-8 col-lg-6 text-center mt-3">
+
+            <img src="shapes/dark-brown-shape-people.png" alt="light-green-shape" class="mb-3" style="width: 100px">
+
+            <strong class="cormorant d-block mb-2 front-page-shape-header">
+                HVAD VI STÅR FOR
+            </strong>
+
+            <p class="inter front-page-shape-text px-3">
+                en masse tekst om alt muligt spændene tihi. en masse tekst om alt muligt spændene tihi.
+            </p>
+
+        </div>
+
+        <div class="col-12 col-md-8 col-lg-6 text-center mt-3">
+
+            <img src="shapes/sage-green-shape-hand.png" alt="light-green-shape" class="mb-3" style="width: 100px">
+
+            <strong class="cormorant d-block mb-2 front-page-shape-header">
+                HVAD VI STÅR FOR
+            </strong>
+
+            <p class="inter front-page-shape-text px-3">
+                en masse tekst om alt muligt spændene tihi. en masse tekst om alt muligt spændene tihi.
+            </p>
+
+        </div>
+
+        <div class="col-12 col-md-8 col-lg-6 text-center mt-3">
+
+            <img src="shapes/light-brown-shape-house.png" alt="light-green-shape" class="mb-3" style="width: 100px">
+
+            <strong class="cormorant d-block mb-2 front-page-shape-header">
+                HVAD VI STÅR FOR
+            </strong>
+
+            <p class="inter front-page-shape-text px-3">
+                en masse tekst om alt muligt spændene tihi. en masse tekst om alt muligt spændene tihi.
+            </p>
+
+        </div>
+
+    </div>
+</div>
+
+<div aria-hidden="true">
+    <img src="waves-phone/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="" >
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
