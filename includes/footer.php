fad4a9777ec3f866f7d210a5be5ef6e9133d2666
louise <asfkls@mgrklm.dk>
2026-05-10 16:08:30 +0200

    footer til lille, mellem og stor skærm (uden funktion)

---
+++ includes/footer.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Vedelsbo</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="robots" content="noindex, nofollow">
+    <link href="/css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/4e7ccd0dde.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<!-- footer til lille skærm -->
+<footer class="bg-dark-green d-block d-md-none">
+    <div class="container ps-4">
+        <div class="row ps-2">
+            <div class="col py-2">
+                <a href="#" class="inter text-off-white">Forside</a>
+            </div>
+            <div class="row">
+                <div class="col py-2">
+                    <a href="#" class="inter text-off-white">Borger</a>
+                </div>
+                <div class="row">
+                    <div class="col py-2">
+                        <a href="#" class="inter text-off-white">Sagsbehandler</a>
+                    </div>
+                    <div class="row">
+                        <div class="col py-2">
+                            <a href="#" class="inter text-off-white">Arbejdsmetoder</a>
+                        </div>
+                        <div class="row">
+                            <div class="col py-2">
+                                <a href="#" class="inter text-off-white">Om Vedelsbo</a>
+                            </div>
+                            <div class="row">
+                                <div class="col py-2">
+                                    <a href="#" class="inter text-off-white">Kontakt</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container ps-4 pb-4">
+        <div class="row pt-4 ps-2">
+            <div class="col py-2 inter text-off-white d-flex align-items-center">
+                <i class="fa-solid fa-phone pe-2"></i>
+                <a href="tel:+4555700011" class="text-off-white text-decoration-none">
+                    55 70 00 11
+                </a>
+            </div>
+        </div>
+
+        <div class="row ps-2">
+            <div class="col py-2 inter text-off-white d-flex align-items-center">
+                <i class="fa-solid fa-envelope pe-2"></i>
+                <a href="mailto:lrh@vedelsbo.dk" class="text-off-white text-decoration-none">
+                    lrh@vedelsbo.dk
+                </a>
+            </div>
+        </div>
+
+        <div class="row ps-2">
+            <div class="col py-2 inter text-off-white d-flex align-items-center">
+                <i class="fa-solid fa-location-dot pe-2"></i>
+
+                <a href="https://www.google.com/maps/search/?api=1&query=Gadevang+23+4700+Næstved"
+                   target="_blank" class="text-off-white text-decoration-none">
+                    Gadevang 23, 4700 Næstved
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container bg-sage-green text-center text-dark-green">
+        <p class="inter m-0 py-2">@ 2026 Vedelsbo. CVR: 37634557</p>
+    </div>
+</footer>
+
+<!-- footer til mellem + stor skærm -->
+<footer class="bg-dark-green pt-4 pt-4 d-none d-md-block">
+    <div class="container text-center">
+        <div class="row align-items-center">
+
+            <div class="col ps-0">
+                <h2 class="allura m-0 text-off-white">
+                    Har du spørgsmål?
+                </h2>
+
+                <p class="inter text-off-white m-0">
+                    Du er altid meget velkommen til at ringe til os
+                    eller skrive os en mail.
+                </p>
+            </div>
+
+            <div class="col text-start">
+                <p class="m-0 pb-2">
+                    <i class="fa-solid fa-phone pe-2 text-off-white"></i>
+
+                    <a href="tel:+4555700011"
+                       class="text-off-white text-decoration-none">
+                        55 70 00 11
+                    </a>
+                </p>
+
+                <p class="m-0 pb-2">
+                    <i class="fa-solid fa-envelope pe-2 text-off-white"></i>
+
+                    <a href="mailto:lrh@vedelsbo.dk"
+                       class="text-off-white text-decoration-none">
+                        lrh@vedelsbo.dk
+                    </a>
+                </p>
+
+                <p class="m-0">
+                    <i class="fa-solid fa-location-dot pe-2 text-off-white"></i>
+
+                    <a href="https://www.google.com/maps/search/?api=1&query=Gadevang+23+4700+Næstved"
+                       target="_blank"
+                       class="text-off-white text-decoration-none">
+                        Gadevang 23, 4700 Næstved
+                    </a>
+                </p>
+            </div>
+
+            <div class="col-3 text-start pe-0">
+                <a href="#" class="d-block inter text-off-white text-decoration-none pb-1">
+                    Forside
+                </a>
+
+                <a href="#" class="d-block inter text-off-white text-decoration-none pb-1">
+                    Borger
+                </a>
+
+                <a href="#" class="d-block inter text-off-white text-decoration-none pb-1">
+                    Sagsbehandler
+                </a>
+
+                <a href="#" class="d-block inter text-off-white text-decoration-none pb-1">
+                    Arbejdsmetoder
+                </a>
+
+                <a href="#" class="d-block inter text-off-white text-decoration-none pb-1">
+                    Om Vedelsbo
+                </a>
+
+                <a href="#" class="d-block inter text-off-white text-decoration-none pb-1">
+                    Kontakt
+                </a>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container-fluid bg-sage-green text-center mt-4">
+        <p class="inter m-0 py-2 text-dark-green">
+            © 2026 Vedelsbo. CVR: 37634557
+        </p>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
