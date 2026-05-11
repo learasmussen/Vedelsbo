d8415545a7cdfa0e6a5c735b3cbe180b5628284b
lea03 <ler002@edu.zealand.dk>
2026-05-11 11:19:13 +0200

    færdiggjort  borger aktiviteter til telefon

---
+++ borger-aktiviteter.php
@@ -0,0 +1,286 @@
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Vedelsbo</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="robots" content="noindex, nofollow">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/4e7ccd0dde.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-background">
+
+<div aria-hidden="true" >
+    <img src="waves-phone/navwave.png" class="waves position-relative w-100" alt="">
+</div>
+
+<div class="container-fluid bg-sage-green ps-4 ps-md-5 pb-md-2 borger-forside-header" >
+
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h1 class="cormorant pe-4 pt-3 fw-bold header-text-cormorant text-center">Aktiviteter</h1>
+        </div>
+        <div class="col-12">
+            <div class="inter header-text-inter pe-4 pe-md-5 me-md-5 text-center">Som beboer på Vedelsbo forventes der at du deltager i hverdagens fælles opgaver i det omfang, du kan. Vi tilpasser sammen omfanget af din deltagelse, til dit funktionsniveau og ud fra dine individuelle behov.</div>
+        </div>
+
+    </div>
+</div>
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/green-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="" >
+</div>
+
+<div class="container-fluid p-0 position-relative" style="z-index: -1000">
+    <img src="images/header-image.jpg" alt="header-image" class="img-fluid header-image-borger d-md-none">
+    <img src="images/header-image-tablet.jpg" alt="header-image" class="img-fluid header-image-borger d-none d-md-block">
+</div>
+
+<div aria-hidden="true" class="beige-big-wave-borger-forside">
+    <img src="waves-phone/beige-big-wave.png" class="waves img-fluid  p-0 m-0" alt="" >
+</div>
+
+<div aria-hidden="true" class="text-end d-md-none decoration-frontpage" style="margin-top: -120px">
+    <img src="header-shapes/green-heart.png" class="decoration-frontpage-image  pe-4 m-0" alt="" style="width: 140px">
+</div>
+
+
+<div class="accordion mt-5 mx-4" id="accordionExample">
+
+    <div class="accordion-item mb-3 border-0 overflow-hidden bg-light-green rounded-5">
+
+        <h2 class="accordion-header">
+
+            <button class="accordion-button collapsed bg-light-green inter fw-bold accordion-header-text shadow"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#naturholdet"
+                    aria-expanded="false"
+                    aria-controls="naturholdet">
+
+                Naturholdet
+
+            </button>
+
+        </h2>
+
+        <div id="naturholdet" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body bg-light-green inter accordion-body-text">
+
+                <p class="mb-3">Tre dage ugentligt i sommerhalvåret og to dage ugentligt i vinterhalvåret deltager beboerne i praktiske opgaver på Vedelsbos udearealer som led i et relations- og aktivitetsbaseret tilbud.</p>
+                <p class="mb-3">Opgaverne kan omfatte oprydning, græsslåning, såning, fejning og øvrig vedligeholdelse af udearealer.</p>
+                <p class="mb-3">Indsatsen tilrettelægges med fokus på struktur, fællesskab og mestring i hverdagen.</p>
+
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="accordion-item mb-3 border-0 overflow-hidden bg-light-green rounded-5">
+
+        <h2 class="accordion-header">
+
+            <button class="accordion-button collapsed bg-light-green inter fw-bold accordion-header-text shadow"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#dyreholdet"
+                    aria-expanded="false"
+                    aria-controls="dyreholdet">
+
+                Dyreholdet
+
+            </button>
+
+        </h2>
+
+        <div id="dyreholdet" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body bg-light-green inter accordion-body-text">
+
+                <p class="mb-3">Vedelsbo har et dyrehold bestående af alpacaer, kat, undulater samt et akvarium.</p>
+                <p class="mb-3">Dyreholdet indgår som en del af de pædagogiske og rehabiliterende aktiviteter.</p>
+                <p class="mb-3">Opgaverne omfatter blandt andet fodring og rengøring, men der lægges samtidig vægt på samvær, nærvær og kontakt med dyrene som en del af den sociale og sansemæssige støtte.</p>
+
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="accordion-item mb-3 border-0 overflow-hidden bg-light-green rounded-5">
+
+        <h2 class="accordion-header">
+
+            <button class="accordion-button collapsed bg-light-green inter fw-bold accordion-header-text shadow"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#creativeSpace"
+                    aria-expanded="false"
+                    aria-controls="creativeSpace">
+
+                Kreativt værksted
+
+            </button>
+
+        </h2>
+
+        <div id="creativeSpace" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body bg-light-green inter accordion-body-text">
+
+                <p class="mb-3">Der afholdes kreativt værksted hver tirsdag i sommerhalvåret samt tirsdag og onsdag i vinterhalvåret.</p>
+                <p class="mb-3">Her arbejdes der med kreative og praktiske aktiviteter ud fra beboernes interesser og ønsker.</p>
+                <p class="mb-3">Der er fokus på medindflydelse, motivation og mulighed for at afprøve forskellige kreative udtryk i et understøttende miljø.</p>
+
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="accordion-item mb-3 border-0 overflow-hidden bg-light-green rounded-5">
+
+        <h2 class="accordion-header">
+
+            <button class="accordion-button collapsed bg-light-green inter fw-bold accordion-header-text shadow"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#places"
+                    aria-expanded="false"
+                    aria-controls="places">
+
+                Værestedet
+
+            </button>
+
+        </h2>
+
+        <div id="places" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body bg-light-green inter accordion-body-text">
+
+                <p class="mb-3">Der er mulighed for, at blive tilknyttet forskellige væresteder, hvor man kan møde ligestillede og på den måde udbygge sit sociale netværk.</p>
+                <p class="mb-3">På værestederne kan man deltage i mange forskellige aktiviteter såsom læderværksted, Køkkenaktiviteter, smykkeværksted og kreativt værksted.</p>
+
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="accordion-item mb-3 border-0 overflow-hidden bg-light-green rounded-5">
+
+        <h2 class="accordion-header">
+
+            <button class="accordion-button collapsed bg-light-green inter fw-bold accordion-header-text shadow"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#udflugter"
+                    aria-expanded="false"
+                    aria-controls="udflugter">
+
+                Udflugter
+
+            </button>
+
+        </h2>
+
+        <div id="udflugter" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body bg-light-green inter accordion-body-text">
+
+                <p class="mb-3">I sommerhalvåret arrangeres løbende aktiviteter og ture “i det grønne”. Der planlægges desuden aktiviteter i nærområdet, særligt i sommerhalvåret.</p>
+                <p class="mb-3">Beboerne har medindflydelse på planlægningen af aktiviteter og udflugter.</p>
+                <p class="mb-3">Der arrangeres både længere dagsture, eksempelvis til Tivoli eller Odense Zoo, samt kortere ture som f.eks. til Karrebæksminde med Sejlads Friheden.</p>
+                <p class="mb-3">Det kan være teltture med overnatning, vandreture med madpakker, kajakture, motionsdag m.v.</p>
+
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="accordion-item mb-3 border-0 overflow-hidden bg-light-green rounded-5">
+
+        <h2 class="accordion-header">
+
+            <button class="accordion-button collapsed bg-light-green inter fw-bold accordion-header-text shadow"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#training"
+                    aria-expanded="false"
+                    aria-controls="training">
+
+                Træning
+
+            </button>
+
+        </h2>
+
+        <div id="training" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body bg-light-green inter accordion-body-text">
+
+                <p class="mb-3">Vedelsbo har fokus på fysisk aktivitet i hverdagen og arbejder ud fra en motionspolitik.</p>
+                <p class="mb-3">Beboerne støttes i at opretholde en aktiv livsstil gennem individuelle og fælles træningsaktiviteter, herunder gåture og planlagt motion. Der kan tilbydes individuel genoptræning ved fysioterapeut i eget hjem eller i klinik.</p>
+                <p class="mb-3">Der er desuden ugentligt tilbud om yoga i huset samt mulighed for yoga med alpacaer i sommerperioden. Yderligere aktiviteter kan omfatte gåture, løb, cykling og roning efter ønske og behov.</p>
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="" >
+</div>
+
+<div class="container d-flex justify-content-center align-items-center bg-light-brown pt-5 read-to-section">
+
+    <div class="row justify-content-center text-center">
+
+        <div class="col-12 mb-4 mt-3">
+            <strong class="cormorant d-block read-to-header">
+                Læs også
+            </strong>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 ps-4">
+            <button class="bg-light-green rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text" style="width: 125px">
+                Aktiviteter
+            </button>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <button class="bg-light-green rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text" style="width: 125px">
+                Årets gang
+            </button>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 ps-4">
+            <button class="bg-light-green rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text" style="width: 125px">
+                Måltider
+            </button>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <button class="bg-light-green rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text" style="width: 125px">
+                Om Vedelsbo
+            </button>
+        </div>
+
+    </div>
+
+</div>
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/light-brown-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="" >
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
