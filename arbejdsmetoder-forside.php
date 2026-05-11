0b7fc638c0f0026e6552012b6f4824f16cfe0e50
lea03 <ler002@edu.zealand.dk>
2026-05-11 13:43:11 +0200

    i gang med arbejdsmetoder-forside.php mobil

--- arbejdsmetoder-forside.php
+++ arbejdsmetoder-forside.php
@@ -1,1 +1,246 @@
-<?php
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Vedelsbo</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="robots" content="noindex, nofollow">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/4e7ccd0dde.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-background">
+
+<?php include 'includes/navbar.php'; ?>
+
+<div aria-hidden="true">
+    <img src="waves-phone/navwave.png" class="waves position-relative w-100" alt="">
+</div>
+
+<div class="container-fluid bg-sage-green ps-4 ps-md-5 pb-md-2 borger-forside-header">
+
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h1 class="cormorant pe-4 pt-3 fw-bold header-text-cormorant text-center">Arbejdsmetoder</h1>
+        </div>
+        <div class="col-12">
+            <div class="inter header-text-inter pe-4 pe-md-5 me-md-5 text-center">Her kan du læse alt om de arbejdsmetoder vi anvender på Vedelsbo.</div>
+        </div>
+
+    </div>
+</div>
+
+<div class="container d-flex justify-content-center align-items-center bg-sage-green pt-5 read-to-section">
+
+    <div class="row justify-content-center text-center mt-5">
+
+
+        <div class="col-6 d-flex justify-content-center mb-3 ps-4">
+            <a href=""
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Terapi
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <a href=""
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Kontaktperson
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 ps-4">
+            <a href=""
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Målsætning
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <a href=""
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Miljøterapi
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <a href=""
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Coaching
+            </a>
+        </div>
+
+    </div>
+
+</div>
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/green-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+
+<div aria-hidden="true" class="text-end d-md-none decoration-frontpage" style="margin-top: -50px">
+    <img src="header-shapes/light-brown-jigsaw.png" class="decoration-frontpage-image  pe-4 m-0" alt=""
+         style="width: 140px">
+</div>
+
+
+<div class="container d-flex justify-content-center align-items-center mt-3">
+
+    <div class="row justify-content-center px-1">
+
+        <div class="col-12 col-lg-8">
+
+            <div class="text-center">
+                <strong class="allura d-block header-text-allura-underpage fw-normal pb-2">
+                    Terapi
+                </strong>
+            </div>
+
+            <strong class="inter fw-bold header-text-inter px-3 pb-1">
+                Kognitiv adfærdsterapi
+            </strong>
+            <p class="inter header-text-inter px-3 mb-3">
+                Personalet uddannes i kognitiv miljøterapi, henholdsvis basisuddannelsen og den videregående uddannelse.
+            </p>
+            <p class="inter header-text-inter px-3 mb-3">
+                I dagligdagen tilstræbes at arbejde med kognitive redskaber i samarbejde med den enkelte beboer, hvor der tages udgangspunkt i beboerens ønsker og funktionsniveau.
+            </p>
+            <p class="inter header-text-inter px-3 mb-3">
+                Formålet med kognitiv miljøterapi er, at støtte den enkelte beboer i at arbejde med at ændre sin adfærd, tanker og følelser og herved finde en mere positiv og hensigtsmæssig tilgang til deres hverdag/liv.
+            </p>
+            <p class="inter header-text-inter px-3 mb-3">
+                Vi arbejder med at lære beboerne at identificere problemer og kritiske situationer, samt sætte nogle realistiske, psykologiske og sociale mål for deres hverdag.
+            </p>
+            <p class="inter header-text-inter px-3 mb-3">
+                Vi arbejder ud fra en række af kognitive metoder.
+            </p>
+        </div>
+
+        <div class="col-12 col-lg-8">
+
+            <p class="inter fw-bold header-text-inter px-3 mb-1">
+                De centrale metoder i kognitiv adfærdsterapi er:
+            </p>
+            <ul>
+            <li class="inter header-text-inter pe-3 mb-1">
+                At hjælpe beboeren med at handle aktivt og udsætte sig selv for vigtige livssituationer snarere end at undgå alt.
+            </li>
+            <li class="inter header-text-inter pe-3 mb-1">
+                At hindre uhensigtsmæssige reaktioner i at finde sted.
+            </li>
+            <li class="inter header-text-inter pe-3 mb-3">
+                At ændre negative tankeprocesser til konstruktive tankeprocesser. Der anvendes sokratisk dialog, udfordring – og tankeeksperimenter.
+            </li>
+            </ul>
+
+            <p class="inter header-text-inter px-3 mb-3">
+                Sokratisk dialog vil sige, at vi er empatisk/lyttende i vores tilgang til beboeren. Ud fra en dagsorden støtter vi beboeren under samtale ved at opsummere og tydeliggøre eksempelvis problemstillinger. Dette gør vi ud fra specifikke og åbne spørgsmål som skal støtte beboeren i selv at finde frem til løsninger. Vi anvender forskellige redskaber (kognitive og adfærdseksperimenter) som beboeren tilbydes at arbejde konkret med i sin hverdag med støtte fra kontaktpersonen.</p>
+            <p class="inter header-text-inter px-3 mb-3">Der følges op/evalueres ved de målrettede samtaler.</p>
+            <p class="inter header-text-inter px-3 mb-3">Beboeren undervises efter behov og ønske i den kognitive referenceramme for herved at tilegne sig en forståelse for metoden.</p>
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+
+<div class="container bg-light-brown d-flex justify-content-center align-items-center pt-5 read-to-section">
+
+    <div class="row justify-content-center px-1">
+
+        <div class="col-12 col-lg-8">
+
+            <div class="text-center">
+                <strong class="allura d-block header-text-allura-underpage fw-normal py-2">
+                    Andre behandlingsmetoder
+                </strong>
+            </div>
+
+            <p class="inter header-text-inter px-3 mb-0">
+                Der tilbydes også: NADA, terapeutisk massage, psykoedukation, mindfullness og Zoneterpi.
+            </p>
+        </div>
+
+    </div>
+</div>
+
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/light-brown-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<div class="container d-flex justify-content-center align-items-center bg-light-brown pt-5 read-to-section">
+
+    <div class="row justify-content-center text-center">
+
+        <div class="col-12 mb-4 mt-3">
+            <strong class="cormorant d-block read-to-header">
+                Læs også
+            </strong>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 ps-4">
+            <a href="/sagsbehandler-values.php"
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Værdier
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <a href="/sagsbehandler-maalgruppe.php"
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Målgruppe
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 ps-4">
+            <a href="/sagsbehandler-visitering.php"
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Visitering
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <a href="/sagsbehandler-praktiske-oplysninger.php"
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Praktisk
+            </a>
+        </div>
+
+    </div>
+
+</div>
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/light-brown-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<?php include 'includes/footer.php'; ?>
+
+</body>
+</html>
+
