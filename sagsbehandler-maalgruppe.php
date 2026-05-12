e0203e2aa5831f20c9676048f863a007611ebb7d
lea03 <ler002@edu.zealand.dk>
2026-05-12 11:19:49 +0200

    tilføjet bootstrap link til alle eksisterende sider

--- sagsbehandler-maalgruppe.php
+++ sagsbehandler-maalgruppe.php
@@ -1,1 +1,162 @@
-<?php
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Vedelsbo</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="robots" content="noindex, nofollow">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/4e7ccd0dde.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-background">
+
+<?php include 'includes/navbar.php'; ?>
+
+<div aria-hidden="true">
+    <img src="waves-phone/navwave.png" class="waves position-relative w-100" alt="">
+</div>
+
+<div class="container-fluid bg-sage-green ps-4 ps-md-5 pb-md-2 borger-forside-header">
+
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h1 class="cormorant pe-4 pt-3 fw-bold header-text-cormorant text-center">Praktiske oplysninger</h1>
+        </div>
+        <div class="col-12">
+            <div class="font-twelve inter pe-4 pe-md-5 me-md-5 text-center">Her kan du læse om vores samarbejdspartnere
+                og om vores økonomi.
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/green-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+
+<div aria-hidden="true" class="text-end d-md-none decoration-frontpage" style="margin-top: -50px">
+    <img src="header-shapes/dark-brown-info.png" class="decoration-frontpage-image  pe-4 m-0" alt=""
+         style="width: 140px">
+</div>
+
+<div class="container d-flex justify-content-center align-items-center mt-3">
+
+    <div class="row justify-content-center px-1">
+
+        <div class="col-12 col-lg-8">
+
+            <div class="text-center">
+                <strong class="allura d-block header-text-allura-underpage fw-normal pb-2">
+                    Samarbejdspartnere
+                </strong>
+            </div>
+
+            <p class="font-twelve inter px-3 mb-3">
+                Alle beboere på Vedelsbo har deres egen praktiserende læge. Vedelsbo samarbejder med Bostedstemaet,
+                Psykiatrien Syd i Vordingborg. Hvis der er behov for at tale med en psykiater aftales det med faste
+                intervaller eller efter behov i samråd med personalet.
+            </p>
+            <p class="font-twelve inter px-3 mb-3">
+                Personalet har i samarbejde med den enkelte beboer, et tæt samarbejde med Bostedsteamet. Hvis der er
+                brug for det, er det muligt at få besøg af enten sin egen læge eller psykiater.
+            </p>
+            <p class="font-twelve inter px-3 mb-3">
+                Vedelsbo har et godt samarbejde med sagsbehandlere, visitatorer, tandlæger, psykologer, fysioterapeuter
+                mm.
+            </p>
+            <p class="font-twelve inter px-3 mb-0">
+                Hver måned er der mulighed for at blive tilknyttet vores faste fodterapeut.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="container d-flex justify-content-center align-items-center mt-3">
+
+    <div class="row justify-content-center px-1">
+
+        <div class="col-12 col-lg-8">
+
+            <div class="text-center">
+                <strong class="allura d-block header-text-allura-underpage fw-normal pb-2">
+                    Økonomi
+                </strong>
+            </div>
+
+            <p class="font-twelve inter px-3 mb-3">
+                Vedelsbo drives som et Anpartsselskab. (ApS)
+            </p>
+            <p class="font-twelve inter px-3 mb-0">
+                Som beboer har man en månedlig egenbetaling som indeholder bl.a. husleje, kost og kørsel.
+                ​Beboerbetalingen udregnes efter størrelsen af boligen og den månedlige indtægt.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+
+<div class="container d-flex justify-content-center align-items-center bg-light-brown pt-5 read-to-section">
+
+    <div class="row justify-content-center text-center">
+
+        <div class="col-12 mb-4 mt-3">
+            <strong class="cormorant d-block read-to-header">
+                Læs også
+            </strong>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 ps-4">
+            <a href="/sagsbehandler-values.php"
+               class="font-twelve bg-light-green text-off-black rounded-pill fw-bold border-0 py-2 px-2 inter d-inline-block text-decoration-none"
+               style="width: 125px">
+                Værdier
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <a href="/sagsbehandler-maalgruppe.php"
+               class="font-twelve bg-light-green text-off-black rounded-pill fw-bold border-0 py-2 px-2 inter d-inline-block text-decoration-none"
+               style="width: 125px">
+                Målgruppe
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 ps-4">
+            <a href="/sagsbehandler-visitering.php"
+               class="font-twelve bg-light-green text-off-black rounded-pill fw-bold border-0 py-2 px-2 inter d-inline-block text-decoration-none"
+               style="width: 125px">
+                Visitering
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <a href="/sagsbehandler-tilsynsrapporter.php"
+               class="font-twelve bg-light-green text-off-black rounded-pill fw-bold border-0 py-2 px-2 inter d-inline-block text-decoration-none"
+               style="width: 125px">
+                Tilsyn
+            </a>
+        </div>
+
+    </div>
+
+</div>
+
+<div aria-hidden="true" class=" green-wavywave-frontpage bg-dark-green">
+    <img src="waves-phone/light-brown-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<?php include 'includes/footer.php'; ?>
+
+</body>
+</html>
--- sagsbehandler-visitering.php
+++ sagsbehandler-visitering.php
@@ -135,6 +135,7 @@
 </div>
 
 <?php include 'includes/footer.php'; ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
