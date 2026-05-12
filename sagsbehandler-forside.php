4e2202e7b6defacb3ac61dbe38228cb80aa6cce8
lea03 <ler002@edu.zealand.dk>
2026-05-12 13:21:06 +0200

    færdig med sagsbehandler-forside.php til mobil

--- sagsbehandler-forside.php
+++ sagsbehandler-forside.php
@@ -1,1 +1,392 @@
-<?php
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Vedelsbo</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="robots" content="noindex, nofollow">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/4e7ccd0dde.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-background">
+
+<?php include 'includes/navbar.php'; ?>
+
+<div aria-hidden="true">
+    <img src="waves-phone/navwave.png" class="waves position-relative w-100" alt="">
+</div>
+
+<div class="container-fluid bg-sage-green ps-4 ps-md-5 pb-md-2 borger-forside-header">
+
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h1 class="cormorant pe-4 pt-3 fw-bold header-text-cormorant">Et socialpsykiatrisk botilbud</h1>
+        </div>
+        <div class="col-12">
+            <div class="inter header-text-inter-italic pe-5 me-5 pe-md-5 me-md-5">Godkendt af Socialtilsyn Øst og drives efter §108 og §107 i serviceloven.
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/green-wavywave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+
+<div class="container bg-light-brown d-flex justify-content-center align-items-center py-5 social-psykiatrisk-botilbud-section">
+
+    <div class="row justify-content-center text-center py-5">
+
+
+        <div class="col-6 d-flex justify-content-center mb-3 ps-4">
+            <a href=""
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Terapi
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <a href=""
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Kontaktperson
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 ps-4">
+            <a href=""
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Målsætning
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <a href=""
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Miljøterapi
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-4 pe-4">
+            <a href=""
+               class="bg-light-green text-off-black rounded-pill fw-bold border-0 py-1 px-2 inter read-to-button-text d-inline-block text-decoration-none"
+               style="width: 125px">
+                Coaching
+            </a>
+        </div>
+
+    </div>
+
+</div>
+
+<div aria-hidden="true" class="beige-big-wave-borger-forside">
+    <img src="waves-phone/beige-big-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<div aria-hidden="true" class="text-end d-md-none decoration-frontpage" style="margin-top: -120px">
+    <img src="header-shapes/dark-brown-logo.png" class="decoration-frontpage-image  pe-4 m-0" alt="" style="width: 140px">
+</div>
+
+<div class="container d-flex justify-content-center align-items-center mt-3">
+
+    <div class="row justify-content-center px-1">
+
+        <div class="col-12 col-lg-8">
+
+            <div class="text-center">
+                <strong class="allura d-block header-text-allura-underpage header-text-allura-underpage-line-height fw-normal pb-2 px-5">
+                    Værdier
+                </strong>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<div class="container d-flex justify-content-center align-items-center mt-3">
+
+    <div class="row justify-content-center px-1">
+        <div class="col-6 d-flex flex-column justify-content-center align-items-center">
+            <img src="shapes/light-green-shape-heart.png" class="mb-1" alt="shape" style="width: 90px">
+            <strong class="cormorant font-fourteen mb-3">ANSVARLIGHED</strong>
+        </div>
+
+        <div class="col-6 d-flex flex-column justify-content-center align-items-center">
+            <img src="shapes/light-green-shape-people.png"  class="mb-1" alt="shape" style="width: 90px">
+            <strong class="cormorant font-fourteen mb-3">OMSORG</strong>
+        </div>
+
+        <div class="col-6 d-flex flex-column justify-content-center align-items-center">
+            <img src="shapes/light-green-shape-hand.png"  class="mb-1" alt="shape" style="width: 90px">
+            <strong class="cormorant font-fourteen mb-3">RESPEKT</strong>
+        </div>
+
+        <div class="col-6 d-flex flex-column justify-content-center align-items-center">
+            <img src="shapes/light-green-shape-house.png"  class="mb-1" alt="shape" style="width: 90px">
+            <strong class="cormorant font-fourteen mb-3">RUMMELIGHED</strong>
+        </div>
+
+        <div class="col-6 d-flex flex-column justify-content-center align-items-center">
+            <img src="shapes/light-green-shape-people.png" class="mb-1" alt="shape" style="width: 90px">
+            <strong class="cormorant font-fourteen mb-3">UDVIKLING</strong>
+        </div>
+
+        <div class="col-6 d-flex flex-column justify-content-center align-items-center">
+            <img src="shapes/light-green-shape-hand.png" class="mb-1" alt="shape" style="width: 90px">
+            <strong class="cormorant font-fourteen mb-3">ÅBENHED</strong>
+        </div>
+
+    </div>
+
+</div>
+
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<div class="container d-flex justify-content-center align-items-center bg-light-brown pt-5 en-beboers-tanker-borger-forside">
+
+    <div class="row justify-content-center w-100 mt-3">
+
+        <div class="col-12 col-md-10 col-lg-8 mb-3">
+
+            <div class="text-center mb-3">
+                <strong class="allura d-block header-text-allura-underpage header-text-allura-underpage-line-height fw-normal pb-2 px-5">
+                    Målgruppe
+                </strong>
+            </div>
+
+            <div class="d-flex align-items-center px-3 mb-3">
+                <i class="fa-solid fa-circle-check text-dark-green me-3 mt-1"></i>
+
+                <div class="font-twelve">
+                    Du er voksen med psykiske udfordringer
+                </div>
+            </div>
+
+            <div class="d-flex align-items-center px-3 mb-3">
+                <i class="fa-solid fa-circle-check text-dark-green me-3 mt-1"></i>
+
+                <div class="font-twelve">
+                    Du har ikke et aktivt misbrug af alkohol, hash, eller andre euforiserende stoffer
+                </div>
+            </div>
+
+            <div class="d-flex align-items-center px-3 mb-3">
+                <i class="fa-solid fa-circle-check text-dark-green me-3 mt-1"></i>
+
+                <div class="font-twelve">
+                    Din adfærd er ikke voldelig og/eller udad reagerende
+                </div>
+            </div>
+
+            <div class="d-flex align-items-center px-3">
+                <i class="fa-solid fa-circle-check text-dark-green me-3 mt-1"></i>
+
+                <div class="font-twelve">
+                    Du vurderes til at kunne indgå i resten af beboergruppen
+                </div>
+            </div>
+
+            <div class="d-flex align-items-center px-3">
+                <i class="fa-solid fa-circle-check text-dark-green me-3 mt-1"></i>
+
+                <div class="font-twelve">
+                    Du er fysisk mobil og anvender ikke hjælpemidler så som kørestol eller rollator
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="col-12 d-flex justify-content-center align-items-center">
+            <a href="/en-beboers-tanker.php"
+               class="font-twelve inter bg-dark-brown text-off-black rounded-pill border-0 py-2 px-3 d-inline-block text-decoration-none">
+                Læs mere om målgruppe
+                <i class="fa-solid fa-angle-right"></i>
+            </a>
+        </div>
+
+    </div>
+
+</div>
+
+<div aria-hidden="true">
+    <img src="waves-phone/light-brown-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+
+<div class="container d-flex justify-content-center align-items-center bg-sage-green pt-5 en-beboers-tanker-borger-forside">
+
+    <div class="row justify-content-center w-100 mt-3">
+
+        <div class="col-12 col-md-10 col-lg-8">
+
+            <div class="text-center mb-3">
+                <strong class="allura d-block header-text-allura-underpage header-text-allura-underpage-line-height fw-normal pb-2 px-5">
+                    Visitering
+                </strong>
+            </div>
+
+            <strong class="font-twelve fw-normal d-block mb-3 mt-3 px-3 inter">
+                I praksis besøger den interesserede borger, evt. pårørende, sagsbehandler fra kommunen og evt. kontaktperson Vedelsbo. Borgeren besøger Vedelsbo i princippet så mange gange der er et gensidigt behov. Under besøget vil daglig leder og evt. kommende kontaktpersoner være tilstede.
+            </strong>
+
+            <div class="col-12 d-flex justify-content-center align-items-center">
+                <a href="/en-beboers-tanker.php"
+                   class="font-twelve inter bg-dark-brown text-off-black rounded-pill border-0 py-2 px-3 d-inline-block text-decoration-none">
+                    Læs mere om visitering
+                    <i class="fa-solid fa-angle-right"></i>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<div aria-hidden="true">
+    <img src="waves-phone/green-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<div class="container d-flex justify-content-center align-items-center bg-light-brown pt-5 en-beboers-tanker-borger-forside">
+
+    <div class="row justify-content-center w-100 mt-3">
+
+        <div class="col-12 col-md-10 col-lg-8">
+
+            <div class="text-center mb-3">
+                <strong class="allura d-block header-text-allura-underpage header-text-allura-underpage-line-height fw-normal pb-2 px-5">
+                    Praktiske oplysninger
+                </strong>
+            </div>
+
+            <strong class="font-twelve fw-normal d-block mb-3 mt-3 px-3 inter">
+                Som beboer har man en månedlig egenbetaling som indeholder bl.a. husleje, kost og kørsel. Beboerbetalingen udregnes efter størrelsen af boligen og den månedlige  indtægt.
+            </strong>
+            <strong class="font-twelve fw-normal d-block mb-3 px-3 inter">
+                Vi samarbejder med borgers egen praktiserende læge, Psykiatrien Syd i Vordingborg, sagsbehandlere, m.m.
+            </strong>
+
+            <div class="col-12 d-flex justify-content-center align-items-center">
+                <a href="/en-beboers-tanker.php"
+                   class="font-twelve inter bg-dark-brown text-off-black rounded-pill border-0 py-2 px-3 d-inline-block text-decoration-none">
+                    Læs mere her
+                    <i class="fa-solid fa-angle-right"></i>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<div aria-hidden="true">
+    <img src="waves-phone/light-brown-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<div class="container d-flex justify-content-center align-items-center bg-sage-green pt-5 en-beboers-tanker-borger-forside">
+
+    <div class="row justify-content-center w-100 mt-3">
+
+        <div class="col-12 col-md-10 col-lg-8">
+
+            <div class="text-center mb-3">
+                <strong class="allura d-block header-text-allura-underpage header-text-allura-underpage-line-height fw-normal pb-2 px-5">
+                    Tilsynsrapporter
+                </strong>
+            </div>
+
+            <strong class="font-twelve fw-normal d-block mb-3 mt-3 px-3 inter">
+                Vedelsbo har årligt et uanmeldt og et anmeldt tilsyn fra Socialtilsyn Øst.
+            </strong>
+
+            <div class="col-12 d-flex justify-content-center align-items-center">
+                <a href="/en-beboers-tanker.php"
+                   class="font-twelve inter bg-dark-brown text-off-black rounded-pill border-0 py-2 px-3 d-inline-block text-decoration-none">
+                    Se vores tilsynsrapporter
+                    <i class="fa-solid fa-angle-right"></i>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<div aria-hidden="true">
+    <img src="waves-phone/green-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+
+<div class="container d-flex justify-content-center align-items-center bg-light-brown pt-5 read-to-section">
+
+    <div class="row justify-content-center text-center">
+
+        <div class="col-12 mb-4 mt-3">
+            <strong class="cormorant d-block read-to-header">
+                Læs også
+            </strong>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 ps-4">
+            <a href="/borger-aktiviteter.php"
+               class="font-twelve bg-light-green text-off-black rounded-pill fw-bold border-0 py-2 px-2 inter d-inline-block text-decoration-none"
+               style="width: 125px">
+                Aktiviteter
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <a href="/borger-fritid.php"
+               class="font-twelve bg-light-green text-off-black rounded-pill fw-bold border-0 py-2 px-2 inter d-inline-block text-decoration-none"
+               style="width: 125px">
+                Fritid
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 ps-4">
+            <a href="/meals.php"
+               class="font-twelve bg-light-green text-off-black rounded-pill fw-bold border-0 py-2 px-2 inter d-inline-block text-decoration-none"
+               style="width: 125px">
+                Måltider
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-center mb-3 pe-4">
+            <a href="/om-vedelsbo.php"
+               class="font-twelve bg-light-green text-off-black rounded-pill fw-bold border-0 py-2 px-2 inter d-inline-block text-decoration-none"
+               style="width: 125px">
+                Om Vedelsbo
+            </a>
+        </div>
+
+    </div>
+
+</div>
+
+<div aria-hidden="true" class=" green-wavywave-frontpage bg-dark-green">
+    <img src="waves-phone/light-brown-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<?php include 'includes/footer.php'; ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
