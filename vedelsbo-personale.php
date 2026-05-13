e64c2e6f72fbe455f5178535174b4c24cd4316d0
lea03 <ler002@edu.zealand.dk>
2026-05-13 11:06:52 +0200

    færdig med vedelsbo-personale.php til mobil

--- vedelsbo-personale.php
+++ vedelsbo-personale.php
@@ -1,1 +1,538 @@
-<?php
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Vedelsbo</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="robots" content="noindex, nofollow">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/4e7ccd0dde.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-background">
+
+<?php include 'includes/navbar.php'; ?>
+
+<div aria-hidden="true">
+    <img src="waves-phone/navwave.png" class="waves position-relative w-100" alt="">
+</div>
+
+<div class="container-fluid bg-sage-green ps-4 ps-md-5 pb-md-2 borger-forside-header">
+
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h1 class="cormorant pe-4 pt-3 fw-bold header-text-cormorant text-center">Vedelsbos personale</h1>
+        </div>
+        <div class="col-12">
+            <div class="font-twelve inter pe-4 pe-md-5 me-md-5 pb-3 text-center">
+                Her kan du møde det personale, der til dagligt arbejder på Vedelsbo. Derudover kan du læse mere om hvordan vi prioriterer arbejdsmiljø, normering, uddannelse og møder.
+            </div>
+        </div>
+
+
+    </div>
+</div>
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/green-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<div class="container-fluid p-0 position-relative" style="z-index: -1000">
+    <img src="images/header-image.jpg" alt="header-image" class="img-fluid header-image-borger d-md-none">
+    <img src="images/header-image-tablet.jpg" alt="header-image" class="img-fluid header-image-borger d-none d-md-block">
+</div>
+
+<div aria-hidden="true" class="beige-big-wave-borger-forside">
+    <img src="waves-phone/beige-big-wave.png" class="waves img-fluid  p-0 m-0" alt="" >
+</div>
+
+
+<div aria-hidden="true" class="text-end d-md-none decoration-frontpage" style="margin-top: -120px">
+    <img src="header-shapes/light-brown-heart.png" class="decoration-frontpage-image  pe-4 m-0" alt="" style="width: 140px">
+</div>
+
+<div class="container d-flex justify-content-center align-items-center mt-3">
+
+    <div class="row justify-content-center px-1">
+
+        <div class="col-12 col-lg-8">
+
+            <div class="text-center">
+                <strong class="allura d-block header-text-allura-underpage fw-normal pb-2">
+                    Ledelsen
+                </strong>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+
+<div class="container d-flex justify-content-center align-items-center mt-3">
+
+    <div class="row justify-content-center px-1 w-100">
+
+        <!-- Lonnie -->
+        <div class="col-6 d-flex justify-content-center mb-4">
+
+            <div class="d-flex flex-column align-items-start">
+
+                <img src="shapes/light-green-shape-user.png"
+                     class="mb-1"
+                     alt="shape"
+                     style="width: 90px">
+
+                <strong class="cormorant font-fourteen fw-bold mb-0">
+                    Lonnie
+                </strong>
+
+                <p class="cormorant font-twelve mb-0">
+                    Daglig leder & stifter
+                </p>
+
+            </div>
+
+        </div>
+
+        <!-- Søren -->
+        <div class="col-6 d-flex justify-content-center mb-4">
+
+            <div class="d-flex flex-column align-items-start">
+
+                <img src="shapes/light-green-shape-user.png"
+                     class="mb-1"
+                     alt="shape"
+                     style="width: 90px">
+
+                <strong class="cormorant font-fourteen fw-bold mb-0">
+                    Søren
+                </strong>
+
+                <p class="cormorant font-twelve mb-0">
+                    Daglig leder & stifter
+                </p>
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+</div>
+
+<div class="container d-flex justify-content-center align-items-center mt-3">
+
+    <div class="row justify-content-center px-1">
+
+        <div class="col-12 col-lg-8">
+
+            <div class="text-center">
+                <strong class="allura d-block header-text-allura-underpage fw-normal pb-2">
+                    Personale
+                </strong>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container d-flex justify-content-center align-items-center mt-3">
+
+    <div class="row justify-content-center px-1 w-100">
+
+        <!-- Thania -->
+        <div class="col-6 d-flex justify-content-center mb-4">
+
+            <div class="d-flex flex-column align-items-start">
+
+                <img src="shapes/light-green-shape-user.png"
+                     class="mb-1"
+                     alt="shape"
+                     style="width: 90px">
+
+                <strong class="cormorant font-fourteen fw-bold mb-0">
+                    Thania
+                </strong>
+
+                <p class="cormorant font-twelve mb-0">
+                    Stilling
+                </p>
+
+            </div>
+
+        </div>
+
+        <!-- Lis -->
+        <div class="col-6 d-flex justify-content-center mb-4">
+
+            <div class="d-flex flex-column align-items-start">
+
+                <img src="shapes/light-green-shape-user.png"
+                     class="mb-1"
+                     alt="shape"
+                     style="width: 90px">
+
+                <strong class="cormorant font-fourteen fw-bold mb-0">
+                    Lis
+                </strong>
+
+                <p class="cormorant font-twelve mb-0">
+                    Stilling
+                </p>
+
+            </div>
+
+        </div>
+
+        <!-- Rene -->
+        <div class="col-6 d-flex justify-content-center mb-4">
+
+            <div class="d-flex flex-column align-items-start">
+
+                <img src="shapes/light-green-shape-user.png"
+                     class="mb-1"
+                     alt="shape"
+                     style="width: 90px">
+
+                <strong class="cormorant font-fourteen fw-bold mb-0">
+                    Rene
+                </strong>
+
+                <p class="cormorant font-twelve mb-0">
+                    Stilling
+                </p>
+
+            </div>
+
+        </div>
+
+        <!-- Kamilla -->
+        <div class="col-6 d-flex justify-content-center mb-4">
+
+            <div class="d-flex flex-column align-items-start">
+
+                <img src="shapes/light-green-shape-user.png"
+                     class="mb-1"
+                     alt="shape"
+                     style="width: 90px">
+
+                <strong class="cormorant font-fourteen fw-bold mb-0">
+                    Kamilla
+                </strong>
+
+                <p class="cormorant font-twelve mb-0">
+                    Stilling
+                </p>
+
+            </div>
+
+        </div>
+
+        <!-- Sille -->
+        <div class="col-6 d-flex justify-content-center mb-4">
+
+            <div class="d-flex flex-column align-items-start">
+
+                <img src="shapes/light-green-shape-user.png"
+                     class="mb-1"
+                     alt="shape"
+                     style="width: 90px">
+
+                <strong class="cormorant font-fourteen fw-bold mb-0">
+                    Sille
+                </strong>
+
+                <p class="cormorant font-twelve mb-0">
+                    Stilling
+                </p>
+
+            </div>
+
+        </div>
+
+        <!-- Charlotte -->
+        <div class="col-6 d-flex justify-content-center mb-4">
+
+            <div class="d-flex flex-column align-items-start">
+
+                <img src="shapes/light-green-shape-user.png"
+                     class="mb-1"
+                     alt="shape"
+                     style="width: 90px">
+
+                <strong class="cormorant font-fourteen fw-bold mb-0">
+                    Charlotte
+                </strong>
+
+                <p class="cormorant font-twelve mb-0">
+                    Stilling
+                </p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<div class="container d-flex justify-content-center align-items-center mt-3">
+
+    <div class="row justify-content-center px-1">
+
+        <div class="col-12 col-lg-8">
+
+            <div class="text-center">
+                <strong class="allura d-block header-text-allura-underpage fw-normal pb-2">
+                    Vikarer
+                </strong>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container d-flex justify-content-center align-items-center mt-3">
+
+    <div class="row justify-content-center px-1 w-100">
+
+        <!-- Hanne -->
+        <div class="col-6 d-flex justify-content-center mb-4">
+
+            <div class="d-flex flex-column align-items-start">
+
+                <img src="shapes/light-green-shape-user.png"
+                     class="mb-1"
+                     alt="shape"
+                     style="width: 90px">
+
+                <strong class="cormorant font-fourteen fw-bold mb-0">
+                    Hanne
+                </strong>
+
+                <p class="cormorant font-twelve mb-0">
+                    Stilling
+                </p>
+
+            </div>
+
+        </div>
+
+        <!-- Lisanne -->
+        <div class="col-6 d-flex justify-content-center mb-4">
+
+            <div class="d-flex flex-column align-items-start">
+
+                <img src="shapes/light-green-shape-user.png"
+                     class="mb-1"
+                     alt="shape"
+                     style="width: 90px">
+
+                <strong class="cormorant font-fourteen fw-bold mb-0">
+                    Lisanne
+                </strong>
+
+                <p class="cormorant font-twelve mb-0">
+                    Stilling
+                </p>
+
+            </div>
+
+        </div>
+
+        <!-- Pia -->
+        <div class="col-6 d-flex justify-content-center mb-4">
+
+            <div class="d-flex flex-column align-items-start">
+
+                <img src="shapes/light-green-shape-user.png"
+                     class="mb-1"
+                     alt="shape"
+                     style="width: 90px">
+
+                <strong class="cormorant font-fourteen fw-bold mb-0">
+                    Pia
+                </strong>
+
+                <p class="cormorant font-twelve mb-0">
+                    Stilling
+                </p>
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+</div>
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<div class="bg-sage-green pt-5 hensyn-og-regler-section" >
+
+
+    <div class="accordion mt-0 py-3 mx-4" id="accordionExample">
+
+        <div class="accordion-item mb-3 border-0 overflow-hidden bg-light-green rounded-5">
+
+            <h2 class="accordion-header">
+
+                <button class="font-fourteen accordion-button collapsed bg-light-green inter fw-bold"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#arbejdsmiljo"
+                        aria-expanded="false"
+                        aria-controls="arbejdsmiljo">
+
+                    Arbejdsmiljø
+
+                </button>
+
+            </h2>
+
+            <div id="arbejdsmiljo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body bg-light-green inter accordion-body-text">
+
+
+                    <div class="mb-3">
+                        <p class="mb-3">Arbejdsmiljøet prioriteres højt i hverdagen, hvor vi har fokus på at trivsel, tillid og tryghed skal være en naturlig del af samarbejdet.</p>
+                        <p class="mb-3">For at sikre bevarelse af et godt arbejdsmiljø afholdes ugentlige evalueringsmøder om samarbejdet i de fælles opgaver der er i huset og i kontaktperson funktionen. Der tages udgangspunkt i vores fælles værdigrundlag som bl. a er respekt, åbenhed og ærlighed. Vi arbejder ud fra teamhjulet med fokus på at have en fælles og ensartet arbejdsgang som er fremmende for det tillidsfulde og gode samarbejde og som ikke mindst skaber tryghed i beboergruppen.</p>
+                        <p class="mb-0">Dårligt samarbejde skaber dårligt arbejdsmiljø. Vi håndterer til stadighed de udfordringer der kan opstå i et samarbejde og tager den svære samtale, så dårligt arbejdsmiljø ikke får grobund i vores daglige arbejdsliv.</p>
+                    </div>
+
+
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="accordion-item mb-3 border-0 overflow-hidden bg-light-green rounded-5">
+
+            <h2 class="accordion-header">
+
+                <button class="font-fourteen accordion-button collapsed bg-light-green inter fw-bold"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#normering"
+                        aria-expanded="false"
+                        aria-controls="normering">
+
+                    Normering
+
+                </button>
+
+            </h2>
+
+            <div id="normering" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body bg-light-green inter accordion-body-text">
+
+                    <div class="mb-3">
+                        <p class="mb-3">Udover daglig leder og administrator/altmuligmand er der ansat 6 kontaktpersoner, 1 køkken-assistent og rengøringsleder og 2 fast tilknyttede vikarer.</p>
+                        <p class="mb-3">Daglig leder er uddannet sygeplejerske og det fastansatte personale har en sundhedsfaglig eller socialpædagogisk uddannelse.</p>
+                        <p class="mb-3">Der er størst personaledækning i dagtimerne, fordelt med 3 – 5 personer.</p>
+                        <p class="mb-0">Dagvagterne møder ind fra kl. 7.30 – 15.00. Aftenvagten møder kl. 14.00 til kl. 22.00. Herefter er der tilkaldevagt fra hjemmet. Såfremt der er behov for det, kan tilkaldevagten være tilstede indenfor ca. 15 minutter og om nødvendigt overnatte på Vedelsbo.</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="accordion-item mb-3 border-0 overflow-hidden bg-light-green rounded-5">
+
+            <h2 class="accordion-header">
+
+                <button class="font-fourteen accordion-button collapsed bg-light-green inter fw-bold"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#uddannelse"
+                        aria-expanded="false"
+                        aria-controls="uddannelse">
+
+                    Uddannelse
+
+                </button>
+
+            </h2>
+
+            <div id="uddannelse" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body bg-light-green inter accordion-body-text">
+
+                    <div class="mb-3">
+                        <p class="mb-3">På Vedelsbo lægges der vægt på trivsel, medindflydelse, uddannelse og ajourføring af viden. Vedelsbo er opsøgende og interesseret i relevante kursustilbud ift. arbejdet på Vedelsbo.</p>
+                        <p class="mb-3">Vedelsbo er derfor også åbne overfor medarbejdernes ønsker om kursustilbud. På møder drøftes den faglige indsats kontinuerligt mhp. at sikre implementering og optimering af det faglige niveau.</p>
+                        <p class="mb-0">Det fastansatte personale tilbydes uddannelse i Kognitiv adfærdsterapi.</p>
+                    </div>
+
+
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="accordion-item mb-0 border-0 overflow-hidden bg-light-green rounded-5">
+
+            <h2 class="accordion-header">
+
+                <button class="font-fourteen accordion-button collapsed bg-light-green inter fw-bold"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#moder"
+                        aria-expanded="false"
+                        aria-controls="moder">
+
+                    Møder
+
+                </button>
+
+            </h2>
+
+            <div id="moder" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body bg-light-green inter accordion-body-text">
+
+                    <div class="mb-3">
+                        <p class="mb-3">Der afholdes morgenmøde hver dag hvor dagens opgaver og aktiviteter planlægges. Der afsættes tid til faglig og personlig sparring.</p>
+                        <p class="mb-3">Der er ugentlig evalueringsmøder hvor alle vigtige emner i det fælles samarbejde bringes op til fælles sparring og løsning.</p>
+                        <p class="mb-3">Der afholdes personalemøder 1 gang om måneden.</p>
+                        <p class="mb-3">Der afholdes intern supervision hver anden måned, samt ekstern supervision ca. 4 gange årligt eller efter behov.</p>
+                        <p class="mb-0">Der planlægges kontinuerlig relevante kurser, uddannelse, temadage mm. Med henblik på optimering af faglighed og personlig udvikling.</p>
+                    </div>
+
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<div aria-hidden="true" class=" green-wavywave-frontpage">
+    <img src="waves-phone/green-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+
+<?php include 'includes/footer.php'; ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
