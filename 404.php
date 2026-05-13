47e619b5539080e1659b33b4741c3292c0744cbc
lea03 <ler002@edu.zealand.dk>
2026-05-13 11:38:47 +0200

    404 side færdig til mobil

---
+++ 404.php
@@ -0,0 +1,67 @@
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Vedelsbo</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="robots" content="noindex, nofollow">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/4e7ccd0dde.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-background">
+
+<div aria-hidden="true" class="fixed-top">
+    <img src="waves-phone/404-top-wave.png" class="waves position-relative w-100" alt="">
+</div>
+
+<div aria-hidden="true" class="d-flex justify-content-end mt-5 pt-5">
+    <img src="logo/404-logo.png" class="waves position-absolute mt-5 pt-3" style="width: 225px" alt="">
+</div>
+
+<div class="container-fluid mt-5 ps-4 ps-md-5 pb-md-2 d-flex justify-content-center align-items-center">
+
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h1 class="inter px-4 pt-3 fw-bold header-text-cormorant text-center">HOV</h1>
+        </div>
+        <div class="col-12">
+            <div class="font-twelve inter px-5 pe-md-5 me-md-5 pb-3 text-center">
+                Denne side findes desværre ikke. Gå tilbage forsiden.
+            </div>
+        </div>
+
+        <div class="col-12 mb-3 d-flex justify-content-center">
+            <a href="/en-beboers-tanker.php"
+               class="font-twelve cormorant bg-dark-brown text-center text-off-black rounded-pill border-0 py-2 px-3 d-inline-block text-decoration-none" style="width: 125px">
+                TILBAGE
+
+            </a>
+        </div>
+
+        <div class="col-12 d-flex justify-content-center">
+            <a href="/en-beboers-tanker.php"
+               class="font-twelve cormorant border border-dark-brown border-2 text-center text-off-black rounded-pill py-2 px-3 d-inline-block text-decoration-none" style="width: 125px">
+                TIL FORSIDEN
+
+            </a>
+        </div>
+
+    </div>
+</div>
+
+<div aria-hidden="true" class="fixed-bottom">
+    <img src="waves-phone/404-bottom-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
