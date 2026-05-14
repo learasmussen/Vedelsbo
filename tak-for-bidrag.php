16bec44dbcb37f3e0178ff51eddb8392c3c2308e
lea03 <ler002@edu.zealand.dk>
2026-05-14 13:00:54 +0200

    tak for bidrag side færdig til mobil

---
+++ tak-for-bidrag.php
@@ -0,0 +1,63 @@
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Vedelsbo</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="robots" content="noindex, nofollow">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/4e7ccd0dde.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-background">
+
+<!--top-wave-->
+<div aria-hidden="true" class="fixed-top ">
+    <img src="waves-phone/tak-for-bidrag-top-wave.png" class="waves position-relative w-100 " alt="">
+</div>
+
+<!--background logo-->
+<div aria-hidden="true" class=" mt-5 pt-5">
+    <img src="logo/tak-for-bidrag-logo.png" class="waves position-absolute start-0 mt-5 pt-5 opacity-75" style="width: 225px" alt="">
+</div>
+
+<!--Text + buttons-->
+<div class="container-fluid mt-5 ps-4 pt-3 ps-md-5 pb-md-2 d-flex justify-content-center align-items-center position-relative">
+
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h1 class="cormorant px-4 pt-3 fw-bold header-text-cormorant text-center">TAK!</h1>
+        </div>
+        <div class="col-12">
+            <div class="font-twelve inter px-5 pe-md-5 me-md-5 pb-3 text-center">
+                Tak for din besked. Vi svarer hurtigst muligt
+            </div>
+        </div>
+
+        <!--Tilbage knap-->
+        <div class="col-12 mb-3 d-flex justify-content-center">
+            <a onclick="history.back()"
+               class="font-twelve fw-bold cormorant bg-dark-green text-center text-off-black rounded-pill border-0 py-2 px-3 d-inline-block text-decoration-none"
+               style="width: 125px">
+                TILBAGE
+
+            </a>
+        </div>
+
+    </div>
+</div>
+
+<!--bottom-wave-->
+<div aria-hidden="true" class="fixed-bottom">
+    <img src="waves-phone/tak-for-bidrag-bottom-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
