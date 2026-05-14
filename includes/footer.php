5329544d43f6a18b10187ccf111bc89c5792d445
lea03 <ler002@edu.zealand.dk>
2026-05-14 12:15:36 +0200

    rettet skalering i footer, samt sat sociale medier ind, men tror det er de forkerte links

--- includes/footer.php
+++ includes/footer.php
@@ -57,7 +57,7 @@
         </div>
     </div>
 
-    <div class="container ps-4 pb-4 position-relative">
+    <div class="container ps-4 pb-0 position-relative">
         <div class="row pt-4 ps-2">
             <div class="col py-2 inter text-off-white d-flex align-items-center">
                 <i class="fa-solid fa-phone pe-2 fa-xs"></i>
@@ -88,32 +88,42 @@
         </div>
     </div>
 
+    <div class="container ps-4 pb-4 position-relative">
+        <div class="row pt-4 ps-2">
+            <div class="col py-2 inter text-off-white d-flex align-items-center">
+                <i class="fa-brands fa-instagram pe-2 fa-xs"></i>
+                <a href="https://www.instagram.com/vedelsbo_botilbud/?hl=da" class="text-off-white text-decoration-none font-twelve">
+                    Instagram
+                </a>
+            </div>
+        </div>
+
+        <div class="row ps-2">
+            <div class="col py-2 inter text-off-white d-flex align-items-center">
+                <i class="fa-brands fa-facebook pe-2 fa-xs"></i>
+                <a href="https://www.facebook.com/profile.php?id=100063647376484&locale=da_DK" class="text-off-white text-decoration-none font-twelve">
+                    FaceBook
+                </a>
+            </div>
+        </div>
+
+    </div>
+
     <div class="container bg-sage-green text-center text-dark-green position-relative">
         <p class="inter m-0 py-2 font-twelve">@ 2026 Vedelsbo. CVR: 37634557</p>
     </div>
 </footer>
 
-<!-- footer til mellem + stor skærm -->
-<footer class="bg-dark-green pt-0 d-none footer d-md-block position-relative" >
+<!-- footer til mellem skærm -->
+<footer class="bg-dark-green pt-0 d-none footer d-md-block d-lg-none position-relative" >
 
     <!--background logo-->
     <div aria-hidden="true" class="d-flex justify-content-end">
-        <img src="/logo/404-logo.png" class="waves position-absolute end-0 bottom-0 mt-5 pt-3" style="width: 250px" alt="">
+        <img src="/logo/404-logo.png" class="waves position-absolute end-0 bottom-0 mt-5 pt-3" style="width: 175px" alt="">
     </div>
 
     <div class="container text-center footer position-relative">
-        <div class="row align-items-center">
-
-            <div class="col ps-0">
-                <h2 class="allura m-0 text-off-white">
-                    Har du spørgsmål?
-                </h2>
-
-                <p class="inter text-off-white m-0">
-                    Du er altid meget velkommen til at ringe til os
-                    eller skrive os en mail.
-                </p>
-            </div>
+        <div class="row align-items-center pt-5 px-5">
 
             <div class="col text-start">
                 <p class="m-0 pb-2">
@@ -134,13 +144,32 @@
                     </a>
                 </p>
 
-                <p class="m-0">
+                <p class="m-0 pb-2">
                     <i class="fa-solid fa-location-dot pe-2 text-off-white"></i>
 
                     <a href="https://www.google.com/maps/search/?api=1&query=Gadevang+23+4700+Næstved"
                        target="_blank"
                        class="text-off-white text-decoration-none">
                         Gadevang 23, 4700 Næstved
+                    </a>
+                </p>
+
+                <p class="m-0 pb-2">
+                    <i class="fa-brands fa-instagram pe-2 text-off-white"></i>
+
+                    <a href="https://www.instagram.com/vedelsbo_botilbud/?hl=da"
+                       class="text-off-white text-decoration-none">
+                        Instagram
+                    </a>
+                </p>
+
+                <p class="m-0">
+                    <i class="fa-brands fa-facebook pe-2 text-off-white"></i>
+
+                    <a href="https://www.facebook.com/profile.php?id=100063647376484&locale=da_DK"
+                       target="_blank"
+                       class="text-off-white text-decoration-none">
+                        FaceBook
                     </a>
                 </p>
             </div>
@@ -170,6 +199,118 @@
                     Kontakt
                 </a>
             </div>
+
+
+
+        </div>
+    </div>
+
+    <div class="container-fluid bg-sage-green text-center mt-4 position-relative">
+        <p class="inter m-0 py-2 text-dark-green">
+            © 2026 Vedelsbo. CVR: 37634557
+        </p>
+    </div>
+</footer>
+
+
+<!-- footer til stor skærm -->
+<footer class="bg-dark-green pt-0 d-none footer d-lg-block position-relative" >
+
+    <!--background logo-->
+    <div aria-hidden="true" class="d-flex justify-content-end">
+        <img src="/logo/404-logo.png" class="waves position-absolute end-0 bottom-0 mt-5 pt-3" style="width: 200px" alt="">
+    </div>
+
+    <div class="container text-center footer position-relative">
+        <div class="row align-items-center pt-5 pb-5">
+
+            <div class="col ps-0">
+                <h2 class="allura m-0 text-off-white">
+                    Har du spørgsmål?
+                </h2>
+
+                <p class="inter text-off-white m-0 px-4">
+                    Du er altid meget velkommen til at ringe til os
+                    eller skrive os en mail.
+                </p>
+            </div>
+
+            <div class="col text-start">
+                <p class="m-0 pb-2">
+                    <i class="fa-solid fa-phone pe-2 text-off-white"></i>
+
+                    <a href="tel:+4555700011"
+                       class="text-off-white text-decoration-none">
+                        55 70 00 11
+                    </a>
+                </p>
+
+                <p class="m-0 pb-2">
+                    <i class="fa-solid fa-envelope pe-2 text-off-white"></i>
+
+                    <a href="mailto:lrh@vedelsbo.dk"
+                       class="text-off-white text-decoration-none">
+                        lrh@vedelsbo.dk
+                    </a>
+                </p>
+
+                <p class="m-0 pb-2">
+                    <i class="fa-solid fa-location-dot pe-2 text-off-white"></i>
+
+                    <a href="https://www.google.com/maps/search/?api=1&query=Gadevang+23+4700+Næstved"
+                       target="_blank"
+                       class="text-off-white text-decoration-none">
+                        Gadevang 23, 4700 Næstved
+                    </a>
+                </p>
+
+                <p class="m-0 pb-2">
+                    <i class="fa-brands fa-instagram pe-2 text-off-white"></i>
+
+                    <a href="https://www.instagram.com/vedelsbo_botilbud/?hl=da"
+                       class="text-off-white text-decoration-none">
+                        Instagram
+                    </a>
+                </p>
+
+                <p class="m-0">
+                    <i class="fa-brands fa-facebook pe-2 text-off-white"></i>
+
+                    <a href="https://www.facebook.com/profile.php?id=100063647376484&locale=da_DK"
+                       target="_blank"
+                       class="text-off-white text-decoration-none">
+                        FaceBook
+                    </a>
+                </p>
+            </div>
+
+            <div class="col-3 text-start pe-0">
+                <a href="#" class="d-block inter text-off-white text-decoration-none pb-1">
+                    Forside
+                </a>
+
+                <a href="#" class="d-block inter text-off-white text-decoration-none pb-1">
+                    Borger
+                </a>
+
+                <a href="#" class="d-block inter text-off-white text-decoration-none pb-1">
+                    Sagsbehandler
+                </a>
+
+                <a href="#" class="d-block inter text-off-white text-decoration-none pb-1">
+                    Arbejdsmetoder
+                </a>
+
+                <a href="#" class="d-block inter text-off-white text-decoration-none pb-1">
+                    Om Vedelsbo
+                </a>
+
+                <a href="#" class="d-block inter text-off-white text-decoration-none pb-1">
+                    Kontakt
+                </a>
+            </div>
+
+
 
         </div>
     </div>
