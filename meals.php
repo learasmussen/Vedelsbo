87651e5fb34f5c1008da507bfcd9996aba5349af
lea03 <ler002@edu.zealand.dk>
2026-05-19 11:16:32 +0200

    skaleret borger-måltider til mellem skærm

--- meals.php
+++ meals.php
@@ -22,13 +22,15 @@
 
 <?php include 'includes/navbar.php'; ?>
 
+<!--LILLE SKÆRM (TELEFON)-->
+
 <!--top-wave-->
-<div aria-hidden="true">
+<div aria-hidden="true" class="d-md-none">
     <img src="waves-phone/navwave.png" class="waves position-relative w-100" alt="">
 </div>
 
 <!--top-info-->
-<div class="container-fluid bg-sage-green ps-4 ps-md-5 pb-md-2 borger-forside-header">
+<div class="container-fluid d-md-none bg-sage-green ps-4 ps-md-5 pb-md-2 borger-forside-header">
 
     <div class="row">
         <div class="col-12 mt-5">
@@ -46,29 +48,27 @@
 </div>
 
 <!--green normal wave-->
-<div aria-hidden="true" class=" green-wavywave-frontpage">
+<div aria-hidden="true" class="d-md-none green-wavywave-frontpage">
     <img src="waves-phone/green-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
 </div>
 
 <!--top image-->
-<div class="container-fluid p-0 position-relative" style="z-index: -1000">
-    <img src="images/header-image.jpg" alt="header-image" class="img-fluid header-image-borger d-md-none">
-    <img src="images/header-image-tablet.jpg" alt="header-image"
-         class="img-fluid header-image-borger d-none d-md-block">
+<div class="container-fluid p-0 d-md-none position-relative" style="z-index: -1000">
+    <img src="images/header-image.jpg" alt="header-image" class="img-fluid header-image-borger">
 </div>
 
 <!--beige big wave-->
-<div aria-hidden="true" class="beige-big-wave-borger-forside">
+<div aria-hidden="true" class="d-md-none beige-big-wave-borger-forside">
     <img src="waves-phone/beige-big-wave.png" class="waves img-fluid  p-0 m-0" alt="">
 </div>
 
 <!--wave shape-->
-<div aria-hidden="true" class="text-end d-md-none decoration-frontpage" style="margin-top: -120px">
+<div aria-hidden="true" class="d-md-none text-end decoration-frontpage" style="margin-top: -120px">
     <img src="header-shapes/green-cutlery.png" class="decoration-frontpage-image  pe-4 m-0" alt="" style="width: 140px">
 </div>
 
 <!--måltidsstruktur-->
-<div class="container d-flex justify-content-center align-items-center mt-3">
+<div class="container d-md-none d-flex justify-content-center align-items-center mt-3">
 
     <div class="row justify-content-center px-1">
 
@@ -90,24 +90,24 @@
 </div>
 
 <!--beige normal wave-->
-<div aria-hidden="true" class=" green-wavywave-frontpage">
+<div aria-hidden="true" class="d-md-none green-wavywave-frontpage">
     <img src="waves-phone/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
 </div>
 
 <!--image-->
-<div class="container-fluid p-0 position-relative" style="z-index: -1000">
+<div class="container-fluid d-md-none p-0 position-relative" style="z-index: -1000">
     <img src="images/header-image.jpg" alt="header-image" class="img-fluid header-image-borger-fritid d-md-none">
     <img src="images/header-image-tablet.jpg" alt="header-image"
          class="img-fluid header-image-borger-fritid d-none d-md-block">
 </div>
 
 <!--beige normal wave-->
-<div aria-hidden="true" class="beige-normal-wave-upside-down-underpage">
+<div aria-hidden="true" class="d-md-none beige-normal-wave-upside-down-underpage">
     <img src="waves-phone/beige-normal-wave-upside-down.png" class="waves img-fluid  p-0 m-0" alt="">
 </div>
 
 <!--mellemmåltider og fællesskab-->
-<div class="container d-flex justify-content-center align-items-center">
+<div class="container d-md-none d-flex justify-content-center align-items-center">
 
     <div class="row justify-content-center px-1">
 
@@ -135,12 +135,12 @@
 </div>
 
 <!--beige normal wave-->
-<div aria-hidden="true" class=" green-wavywave-frontpage">
+<div aria-hidden="true" class="d-md-none green-wavywave-frontpage">
     <img src="waves-phone/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
 </div>
 
 <!--read too section-->
-<div class="container d-flex justify-content-center align-items-center bg-light-brown pt-5 read-to-section">
+<div class="container d-md-none d-flex justify-content-center align-items-center bg-light-brown pt-5 read-to-section">
 
     <div class="row justify-content-center text-center">
 
@@ -187,9 +187,178 @@
 </div>
 
 <!--bottom-wave-->
-<div aria-hidden="true" class=" green-wavywave-frontpage bg-dark-green">
+<div aria-hidden="true" class="d-md-none green-wavywave-frontpage bg-dark-green">
     <img src="waves-phone/light-brown-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
 </div>
+
+
+
+<!--MELLEM SKÆRM (TABLET)-->
+
+<!--top-wave - TABLET-->
+<div aria-hidden="true" class="d-none d-md-block">
+    <img src="waves-tablet/nav-wave.png" class="waves position-relative w-100" alt="">
+</div>
+
+<!--image header - TABLET-->
+<div class="container-fluid d-none d-md-block p-0 position-relative" style="z-index: -1000">
+    <img src="images/header-image-tablet.jpg" alt="header-image" class="img-fluid header-image-borger">
+</div>
+
+<!--green big wave - TABLET-->
+<div aria-hidden="true" class="d-none d-md-block green-wavywave-frontpage" style="margin-top: -275px">
+    <img src="waves-tablet/green-big-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--wave shape - TABLET-->
+<div aria-hidden="true" class="text-end d-none d-md-block decoration-frontpage" style="margin-top: -210px">
+    <img src="header-shapes/green-cutlery.png" class="decoration-frontpage-image  pe-4 m-0" alt="" style="width: 180px">
+</div>
+
+<!--top-info - TABLET-->
+<div class="container-fluid d-none d-md-flex justify-content-center align-items-center bg-sage-green ps-4 pt-md-0 ps-md-0 pb-md-2 borger-forside-header" style="margin-top: 110px" >
+
+    <div class="row d-md-flex justify-content-center align-items-center">
+        <div class="col-10 mt-0">
+            <h1 class="cormorant  fw-bold header-text-cormorant text-center">Måltider</h1>
+        </div>
+        <div class="col-10 mb-3">
+            <div class="font-twelve font-sixteen px-5 inter  text-center">
+                På Vedelsbo lægges der vægt på sund, varieret og velsmagende kost med afsæt i husets kostpolitik. Der er fokus på, at måltider og fælles kaffestunder understøtter sociale relationer og fællesskab, hvor beboere og personale har mulighed for dialog, samvær og gensidig kontakt.
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<!--green normal wave - TABLET-->
+<div aria-hidden="true" class="d-none d-md-block green-wavywave-frontpage">
+    <img src="waves-tablet/green-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--måltidsstruktur - TABLET-->
+<div class="container d-none d-md-flex justify-content-center align-items-center mt-3">
+
+    <div class="row justify-content-center px-1">
+
+        <div class="col-12 col-lg-8">
+
+            <div class="text-center">
+                <strong class="allura d-block header-text-allura-underpage fw-normal mt-5 pb-2">
+                    Måltidsstruktur
+                </strong>
+            </div>
+
+            <p class="font-twelve font-sixteen inter px-5 mb-0">
+                Beboerne i egen lejlighed forventes at indtage morgenmad i egen bolig. I boliger med fælles køkken
+                tilbydes morgenbuffet. Frokost og aftensmad indtages som udgangspunkt i fællesskab i fælleshuset på
+                faste tidspunkter.
+            </p>
+        </div>
+    </div>
+</div>
+
+<!--beige normal wave - TABLET-->
+<div aria-hidden="true" class="d-none d-md-block green-wavywave-frontpage">
+    <img src="waves-tablet/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--image - TABLET-->
+<div class="container-fluid d-none d-md-block p-0 position-relative" style="z-index: -1000">
+    <img src="images/header-image-tablet.jpg" alt="header-image"
+         class="img-fluid header-image-borger-fritid d-none d-md-block">
+</div>
+
+<!--beige normal wave - TABLET-->
+<div aria-hidden="true" class="d-none d-md-block beige-normal-wave-upside-down-underpage">
+    <img src="waves-tablet/beige-big-wave-upside-down.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--mellemmåltider og fællesskab - TABLET-->
+<div class="container d-none d-md-flex justify-content-center align-items-center">
+
+    <div class="row justify-content-center px-1">
+
+        <div class="col-12 ">
+
+            <div class="text-center">
+                <strong class="allura d-block header-text-allura-underpage  fw-normal pb-2">
+                    Mellemmåltider og fællesskab
+                </strong>
+            </div>
+
+            <p class="font-sixteen inter px-5 mb-3">
+                Der er altid adgang til frugt og løbende sunde mellemmåltider i hverdagen. Beboerne har adgang til en
+                espressomaskine i fælleshuset, hvor de kan tilberede kaffe efter eget valg. I weekenderne er der
+                mulighed for mere fleksible kostvaner, hvor der eksempelvis bages kage eller tilberedes desserter med
+                støtte fra personalet efter ønske.
+            </p>
+
+            <p class=" font-sixteen inter px-5 mb-0">
+                I vinterhalvåret tilbydes deltagelse i madgruppe med fokus på sund kost, madlavning og kostforståelse.
+            </p>
+
+        </div>
+    </div>
+</div>
+
+<!--beige normal wave - TABLET-->
+<div aria-hidden="true" class="d-none d-md-block green-wavywave-frontpage">
+    <img src="waves-tablet/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--read too section - TABLET-->
+<div class="container-fluid d-none d-md-flex justify-content-center align-items-center bg-light-brown pt-5 read-to-section">
+
+    <div class="row justify-content-center text-center mt-5">
+
+        <div class="col-12 mb-4 mt-3">
+            <strong class="cormorant d-block read-to-header">
+                Læs også
+            </strong>
+        </div>
+
+        <div class="col-6 d-flex justify-content-end mb-3 ps-4 pe-2">
+            <a href="borger-fritid.php"
+               class="font-twelve font-sixteen bg-light-green text-off-black rounded-pill fw-bold border-0 py-2 px-2 inter d-inline-block text-decoration-none"
+               style="width: 275px">
+                Fritid
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-start mb-3 pe-4 ps-2">
+            <a href="a-year.php"
+               class="font-twelve font-sixteen bg-light-green text-off-black rounded-pill fw-bold border-0 py-2 px-2 inter d-inline-block text-decoration-none"
+               style="width: 275px">
+                Årets gang
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-end mb-3 ps-4 pe-2">
+            <a href="meals.php"
+               class="font-twelve font-sixteen bg-light-green text-off-black rounded-pill fw-bold border-0 py-2 px-2 inter d-inline-block text-decoration-none"
+               style="width: 275px">
+                Måltider
+            </a>
+        </div>
+
+        <div class="col-6 d-flex justify-content-start mb-3 pe-4 ps-2">
+            <a href="om-vedelsbo.php"
+               class="font-twelve font-sixteen bg-light-green text-off-black rounded-pill fw-bold border-0 py-2 px-2 inter d-inline-block text-decoration-none"
+               style="width: 275px">
+                Om Vedelsbo
+            </a>
+        </div>
+
+    </div>
+
+</div>
+
+<!--bottom-wave - TABLET-->
+<div aria-hidden="true" class="d-none d-md-block green-wavywave-frontpage bg-dark-green">
+    <img src="waves-tablet/light-brown-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
 
 <?php include 'includes/footer.php'; ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
