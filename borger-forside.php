38aaed527b3d05c124864162e6dc88774fbd8c42
lea03 <ler002@edu.zealand.dk>
2026-05-20 12:53:21 +0200

    igang med borger forside

--- borger-forside.php
+++ borger-forside.php
@@ -22,18 +22,15 @@
 
 <?php include 'includes/navbar.php'; ?>
 
+<!--LILLE SKÆRM (TELEFON)-->
+
 <!--top-wave-->
 <div aria-hidden="true" class="d-md-none">
     <img src="waves-phone/navwave.png" class="waves position-relative w-100" alt="">
 </div>
 
-<!--top-wave md-->
-<div aria-hidden="true" class="d-none d-md-block">
-    <img src="waves-tablet/nav-wave.png" class="waves position-relative w-100" alt="">
-</div>
-
 <!--top-info-->
-<div class="container-fluid bg-sage-green ps-4 ps-md-5 pb-md-2 borger-forside-header">
+<div class="container-fluid d-md-none bg-sage-green ps-4 ps-md-5 pb-md-2 borger-forside-header">
 
     <div class="row">
         <div class="col-12 mt-5">
@@ -49,12 +46,12 @@
 </div>
 
 <!--green wavy wave-->
-<div aria-hidden="true" class="">
+<div aria-hidden="true" class="d-md-none">
     <img src="waves-phone/green-wavywave.png" class="waves img-fluid  p-0 m-0" alt="">
 </div>
 
 <!--top-image-->
-<div class="container-fluid p-0 position-relative" style="z-index: -1000">
+<div class="container-fluid d-md-none p-0 position-relative" style="z-index: -1000">
     <img src="images/header-image.jpg" alt="header-image" class="img-fluid header-image-borger d-md-none">
     <img src="images/header-image-tablet.jpg" alt="header-image"
          class="img-fluid header-image-borger d-none d-md-block">
@@ -65,13 +62,8 @@
     <img src="waves-phone/beige-big-wave.png" class="waves img-fluid  p-0 m-0" alt="">
 </div>
 
-<!--beige big wave-->
-<div aria-hidden="true" class="beige-big-wave-borger-forside d-none d-md-block">
-    <img src="waves-tablet/beige-big-wave.png" class="waves img-fluid  p-0 m-0" alt="">
-</div>
-
 <!--shapes-->
-<div class="container d-flex justify-content-center align-items-center hverdag-shapes-borger-forside">
+<div class="container d-flex d-md-none justify-content-center align-items-center hverdag-shapes-borger-forside">
     <div class="row justify-content-center">
 
         <!--aktiviteter-->
@@ -187,13 +179,8 @@
     <img src="waves-phone/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
 </div>
 
-<!--beige normal wave-->
-<div aria-hidden="true" class="d-none d-md-block bg-sage-green">
-    <img src="waves-tablet/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
-</div>
-
 <!--citat fra jette-->
-<div class="container-fluid mt-0 d-flex justify-content-center align-items-center bg-sage-green  ">
+<div class="container-fluid mt-0 d-flex d-md-none justify-content-center align-items-center bg-sage-green  ">
 
     <div class="row justify-content-center text-center w-100 mt-3">
 
@@ -227,9 +214,383 @@
     <img src="waves-phone/green-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
 </div>
 
-<!--bottom-wave-->
-<div class="bg-dark-green d-none d-md-block" aria-hidden="true">
+
+
+
+<!--MELLEM SKÆRM (TABLET)-->
+
+<!--top-wave - TABLET-->
+<div aria-hidden="true" class="d-none d-md-block d-lg-none">
+    <img src="waves-tablet/nav-wave.png" class="waves position-relative w-100" alt="">
+</div>
+
+<!--top-info - TABLET-->
+<div class="container-fluid d-none d-md-block d-lg-none bg-sage-green ps-4 ps-md-5 pb-md-2 borger-forside-header">
+
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h1 class="cormorant pe-4 pt-3 fw-bold header-text-cormorant">En hverdag med mening og muligheder</h1>
+        </div>
+        <div class="col-12 col-md-10 pt-md-3">
+            <div class="inter font-twelve font-eightteen pe-4 pe-md-5 me-md-5">På Vedelsbo skaber vi rammer for en tryg og
+                meningsfuld hverdag med fællesskab, aktiviteter, nærvær og plads til personlig udvikling.
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<!--green wavy wave - TABLET-->
+<div aria-hidden="true" class="d-none d-md-block d-lg-none">
+    <img src="waves-phone/green-wavywave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--top-image - TABLET-->
+<div class="container-fluid d-none d-md-block d-lg-none p-0 position-relative" style="z-index: -1000">
+    <img src="images/header-image.jpg" alt="header-image" class="img-fluid header-image-borger d-md-none">
+    <img src="images/header-image-tablet.jpg" alt="header-image"
+         class="img-fluid header-image-borger d-none d-md-block">
+</div>
+
+<!--beige big wave - TABLET-->
+<div aria-hidden="true" class="beige-big-wave-borger-forside d-none d-md-block d-lg-none">
+    <img src="waves-tablet/beige-big-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--shapes - TABLET-->
+<div class="container d-none d-md-flex d-lg-none justify-content-center align-items-center hverdag-shapes-borger-forside">
+    <div class="row justify-content-center">
+
+        <!--aktiviteter-->
+        <div class="col-12 col-md-6 col-lg-6 text-center mt-3">
+
+            <div class="shape-box-1">
+
+                <div class="shape-content">
+                    <strong class="cormorant d-block mb-2 mt-4 front-page-shape-header">
+                        AKTIVITETER
+                    </strong>
+
+                    <p class="inter px-4 mx-2 mb-0 font-fourteen">
+                        På Vedelsbo er aktiviteter en vigtig del af hverdagen. Læs om alle vores aktiviteter herunder.
+                    </p>
+
+                    <div class="mt-3">
+                        <a href="borger-aktiviteter.php"
+                           class="btn bg-off-white rounded-5 px-2 py-1">
+                            <i class="fa-solid fa-angle-right"></i>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <!--fritid-->
+        <div class="col-12 col-md-6 col-lg-6 text-center mt-3">
+
+            <div class="shape-box-2">
+
+                <div class="shape-content">
+                    <strong class="cormorant d-block mb-2 front-page-shape-header">
+                        FRITID
+                    </strong>
+
+                    <p class="inter px-4 mx-2 mb-0 font-fourteen">
+                        Vi støtter beboerne i at skabe en meningsfuld fritid med plads til både interesser, afslapning
+                        og sociale oplevelser.
+                    </p>
+
+                    <div class="mt-3">
+                        <a href="borger-fritid.php"
+                           class="btn bg-off-white rounded-5 px-2 py-1">
+                            <i class="fa-solid fa-angle-right"></i>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <!--årets gang-->
+        <div class="col-12 col-md-6 col-lg-6 text-center mt-3">
+
+            <div class="shape-box-3">
+
+                <div class="shape-content">
+                    <strong class="cormorant d-block mb-2 mt-4 front-page-shape-header">
+                        ÅRETS GANG
+                    </strong>
+
+                    <p class="inter px-4 mx-2 mb-0 font-fourteen">
+                        Traditioner og fællesskab er en vigtig del af livet på Vedelsbo. Vi fejrer højtider og
+                        arrangerer aktiviteter året rundt sammen med beboerne.
+                    </p>
+
+                    <div class="mt-3">
+                        <a href="a-year.php"
+                           class="btn bg-off-white rounded-5 px-2 py-1">
+                            <i class="fa-solid fa-angle-right"></i>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <!--måltider-->
+        <div class="col-12 col-md-6 col-lg-6 text-center mt-3 ">
+
+            <div class="shape-box-4">
+
+                <div class="shape-content">
+                    <strong class="cormorant d-block mb-2 mt-4 front-page-shape-header">
+                        MÅLTIDER
+                    </strong>
+
+                    <p class="inter px-4 mx-2 mb-0 font-fourteen">
+                        Måltiderne på Vedelsbo handler om både sund kost og hyggeligt samvær. Vi skaber trygge rammer
+                        omkring fællesskab og gode madoplevelser.
+                    </p>
+
+                    <div class="mt-3">
+                        <a href="meals.php"
+                           class="btn bg-off-white rounded-5 px-2 py-1">
+                            <i class="fa-solid fa-angle-right"></i>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--beige normal wave - TABLET-->
+<div aria-hidden="true" class="d-none d-md-block d-lg-none bg-sage-green">
+    <img src="waves-tablet/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--citat fra jette - TABLET-->
+<div class="container-fluid mt-0 d-none d-md-flex d-lg-none justify-content-center align-items-center bg-sage-green  ">
+
+    <div class="row justify-content-center text-center w-100 mt-3">
+
+        <div class="col-12 col-md-10 col-lg-8">
+
+            <strong class="cormorant d-block mb-3 pt-md-5 en-beboers-tanker-borger-forside-text-italic">
+                “Den omsorg vi får gør hverdagen lys, den nærhed vi mærker gør hjertet varmt, livet blir levende og det
+                gør forskellen.”
+            </strong>
+
+            <p class="inter mb-4 en-beboers-tanker-borger-forside-text">
+                - Jette Bernt Rasmussen
+            </p>
+
+            <div>
+                <a href="en-beboers-tanker.php"
+                   class="font-twelve font-sixteen inter bg-dark-brown text-off-black rounded-pill border-0 py-2 px-3 d-inline-block text-decoration-none">
+                    En beboers tanker
+                    <i class="fa-solid fa-angle-right"></i>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!--bottom-wave - TABLET-->
+<div class="bg-dark-green d-none d-md-block d-lg-none" aria-hidden="true">
     <img src="waves-tablet/green-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+
+
+
+<!--STOR SKÆRM (DESKTOP)-->
+
+<!--top-wave - DESKTOP-->
+<div aria-hidden="true" class="d-none d-lg-block">
+    <img src="waves-desktop/nav-wave.png" class="waves position-relative w-100" alt="">
+</div>
+
+<!--image header - DESKTOP-->
+<div class="container-fluid d-none d-lg-block p-0 position-relative" style="z-index: -1000">
+    <img src="images/personale_flowers_large.JPG"
+         alt="header-image" class="img-fluid header-image-borger">
+</div>
+
+<!--beige wave upside down - DESKTOP-->
+<div aria-hidden="true" class="d-none d-lg-block green-wavywave-frontpage" style="margin-top: -80px">
+    <img src="waves-desktop/beige-normal-wave-upside-down.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--top-info - TABLET-->
+<div class="container-fluid d-none d-lg-block justify-content-center align-items-center ps-4 pt-md-0 ps-md-0 pb-md-2 borger-forside-header mt-3"  >
+
+    <div class="row d-md-flex justify-content-center align-items-center">
+        <div class="col-10 mt-0">
+            <h1 class="cormorant mb-4  fw-bold header-text-cormorant text-center">En hverdag med mening og muligheder</h1>
+        </div>
+        <div class="col-10 mb-3">
+            <div class="font-twelve font-sixteen px-5 inter  text-center">
+                På Vedelsbo skaber vi rammer for en tryg og meningsfuld hverdag med fællesskab, aktiviteter, nærvær og plads til personlig udvikling.            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<!--beige normal wave - DESKTOP-->
+<div aria-hidden="true" class="d-none d-lg-block bg-light-green green-wavywave-frontpage">
+    <img src="waves-desktop/beige-normal-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--aktiviteter-->
+<div class="container-fluid d-none d-lg-flex justify-content-center align-items-center bg-light-green">
+    <div class="row d-flex justify-content-center align-items-center">
+        
+        <div class="col-6 ps-5 pb-0">
+            <img src="images/round_placeholder.png" alt="" class="img-fluid" style="width: 400px;">
+        </div>
+
+        <div class="col-6">
+            <strong class="cormorant d-block mb-3 mt-0 front-page-shape-header">
+                AKTIVITETER
+            </strong>
+
+            <p class="inter px-0 mx-0 mb-3 font-eightteen">
+                På Vedelsbo er aktiviteter en vigtig del af hverdagen og understøtter både trivsel, fællesskab og personlig udvikling. Beboerne kan deltage i kreative værksteder, natur- og dyrehold, motion, bålhygge og fælles udflugter i rolige og trygge rammer.
+            </p>
+
+            <div>
+                <a href="borger-aktiviteter.php"
+                   class="font-twelve font-sixteen inter bg-dark-brown text-off-black rounded-pill border-0 py-2 px-3 d-inline-block text-decoration-none">
+                    Læs mere om aktiviteter
+                    <i class="fa-solid fa-angle-right"></i>
+                </a>
+            </div>
+
+        </div>
+        
+    </div>
+</div>
+
+<!--beige normal wave - DESKTOP-->
+<div aria-hidden="true" class="d-none d-lg-block bg-off-white green-wavywave-frontpage">
+    <img src="waves-desktop/normal-dark-green-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--fritid-->
+<div class="container-fluid d-none d-lg-flex justify-content-center align-items-center bg-off-white">
+    <div class="row d-flex justify-content-center align-items-center my-3">
+
+        <div class="col-6 ps-5">
+            <strong class="cormorant d-block mb-3 mt-0 front-page-shape-header">
+                FRITID
+            </strong>
+
+            <p class="inter px-0 mx-0 mb-3 font-eightteen">
+                Vi støtter beboerne i at skabe en meningsfuld fritid med plads til både interesser, afslapning og sociale oplevelser. Der er blandt andet mulighed for både kreative aktiviteter, gåture, sociale arrangementer, biografture og rolige stunder, hvor fællesskab og trivsel er i fokus.
+            </p>
+
+            <div>
+                <a href="borger-fritid.php"
+                   class="font-twelve font-sixteen inter bg-dark-brown text-off-black rounded-pill border-0 py-2 px-3 d-inline-block text-decoration-none">
+                    Læs mere om fritid
+                    <i class="fa-solid fa-angle-right"></i>
+                </a>
+            </div>
+
+        </div>
+
+        <div class="col-6 ps-5 pb-0">
+            <img src="images/round_placeholder.png" alt="" class="img-fluid" style="width: 400px;">
+        </div>
+
+
+    </div>
+</div>
+
+<!--beige normal wave - DESKTOP-->
+<div aria-hidden="true" class="d-none d-lg-block bg-sage-green green-wavywave-frontpage">
+    <img src="waves-desktop/off-white-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--årets gang-->
+<div class="container-fluid d-none d-lg-flex justify-content-center align-items-center bg-sage-green">
+    <div class="row d-flex justify-content-center align-items-center">
+
+        <div class="col-6 ps-5 pb-0">
+            <img src="images/round_placeholder.png" alt="" class="img-fluid" style="width: 400px;">
+        </div>
+
+        <div class="col-6">
+            <strong class="cormorant d-block mb-3 mt-0 front-page-shape-header">
+                ÅRETS GANG
+            </strong>
+
+            <p class="inter px-0 mx-0 mb-3 font-eightteen">
+                Årets gang på Vedelsbo er præget af traditioner, fællesskab og hyggelige oplevelser. Vi afholder blandt andet sommerfest, julearrangementer, ture og sociale aktiviteter, hvor beboerne har medindflydelse og mulighed for at skabe gode minder sammen.
+            </p>
+
+            <div>
+                <a href="a-year.php"
+                   class="font-twelve font-sixteen inter bg-dark-brown text-off-black rounded-pill border-0 py-2 px-3 d-inline-block text-decoration-none">
+                    Læs mere om årets gang
+                    <i class="fa-solid fa-angle-right"></i>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<!--beige normal wave - DESKTOP-->
+<div aria-hidden="true" class="d-none d-lg-block bg-light-brown green-wavywave-frontpage">
+    <img src="waves-desktop/normal-green-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+<!--måltider-->
+<div class="container-fluid d-none d-lg-flex justify-content-center align-items-center bg-light-brown">
+    <div class="row d-flex justify-content-center align-items-center my-3">
+
+        <div class="col-6 ps-5">
+            <strong class="cormorant d-block mb-3 mt-0 front-page-shape-header">
+                MÅLTIDER
+            </strong>
+
+            <p class="inter px-0 mx-0 mb-3 font-eightteen">
+                På Vedelsbo lægger vi vægt på sund, varieret og hjemmelavet mad, hvor måltiderne også fungerer som et socialt samlingspunkt. Her er der plads til nærvær, dialog og fællesskab i rolige og hyggelige omgivelser.
+            </p>
+
+            <div>
+                <a href="meals.php"
+                   class="font-twelve font-sixteen inter bg-dark-brown text-off-black rounded-pill border-0 py-2 px-3 d-inline-block text-decoration-none">
+                    Læs mere om måltider
+                    <i class="fa-solid fa-angle-right"></i>
+                </a>
+            </div>
+
+        </div>
+
+        <div class="col-6 ps-5 pb-0">
+            <img src="images/round_placeholder.png" alt="" class="img-fluid" style="width: 400px;">
+        </div>
+
+
+    </div>
+</div>
+
+<!--beige normal wave - DESKTOP-->
+<div aria-hidden="true" class="d-none d-lg-block bg-beige green-wavywave-frontpage">
+    <img src="waves-desktop/normal-light-brown-wave.png" class="waves img-fluid  p-0 m-0" alt="">
 </div>
 
 <?php include 'includes/footer.php'; ?>
