a8638759281e82fd6d1171550c15f80a3d9f1ad2
lea03 <ler002@edu.zealand.dk>
2026-05-21 11:19:44 +0200

    færdig med 404 til alle skærme

--- 404.php
+++ 404.php
@@ -78,17 +78,17 @@
 <!--MELLEM SKÆRM (TABLET)-->
 
 <!--top-wave-->
-<div aria-hidden="true" class="fixed-top d-none d-md-block">
+<div aria-hidden="true" class="fixed-top d-none d-md-block d-lg-none">
     <img src="waves-tablet/404-top-wave.png" class="waves position-relative w-100" alt="">
 </div>
 
 <!--background logo-->
-<div aria-hidden="true" class="end-0 mt-5 pt-5 d-none d-md-block">
-    <img src="logo/404-logo.png" class="waves position-absolute mt-5 pt-3" style="width: 225px" alt="">
+<div aria-hidden="true" class=" mt-5 pt-5 d-none d-md-block d-lg-none">
+    <img src="logo/404-logo.png" class="end-0 waves position-absolute mt-5 pt-3" style="width: 275px" alt="">
 </div>
 
 <!-- Text + buttons -->
-<div class="container-fluid d-none d-md-block mt-5 ps-4 ps-md-5 pb-md-2 d-flex justify-content-center align-items-center position-relative">
+<div class="container-fluid d-none d-md-block d-lg-none mt-5 ps-4 ps-md-5 pb-md-2 d-flex justify-content-center align-items-center position-relative">
 
     <div class="row">
 
@@ -138,8 +138,76 @@
 </div>
 
 <!--bottom-wave-->
-<div aria-hidden="true" class="fixed-bottom d-none d-md-block">
+<div aria-hidden="true" class="fixed-bottom d-none d-md-block d-lg-none">
     <img src="waves-tablet/404-bottom-wave.png" class="waves img-fluid  p-0 m-0" alt="">
+</div>
+
+
+<!--STOR SKÆRM (DESKTOP)-->
+
+<!--top-wave-->
+<div aria-hidden="true" class="fixed-top d-none d-lg-block">
+    <img src="waves-desktop/404-top-wave.png" class="waves position-relative w-100" alt="">
+</div>
+
+<!--background logo-->
+<div aria-hidden="true" class=" mt-5 pt-5 d-none d-lg-block">
+    <img src="logo/404-logo.png" class="end-0 waves position-absolute mt-5 pt-3" style="width: 275px" alt="">
+</div>
+
+<!-- Text + buttons -->
+<div class="container-fluid d-none d-lg-block mt-5 ps-4 ps-md-5 pb-md-2 d-flex justify-content-center align-items-center position-relative">
+
+    <div class="row">
+
+        <!-- overskrift -->
+        <div class="col-12 mt-5">
+
+            <h1 class="inter px-4 pt-3 fw-bold header-text-cormorant text-center">
+                HOV
+            </h1>
+
+        </div>
+
+        <!-- tekst -->
+        <div class="col-12">
+
+            <div class="font-sixteen inter px-5 pb-3 text-center">
+                Denne side findes desværre ikke. Gå tilbage forsiden.
+            </div>
+
+        </div>
+
+        <!-- knapper -->
+        <div class="col-12 mb-3 d-flex justify-content-center gap-2">
+
+            <!-- tilbage -->
+            <a onclick="history.back()"
+               class="font-sixteen inter bg-dark-brown text-center text-off-black rounded-pill border-0 py-2 px-3 d-inline-block text-decoration-none"
+               style="width: 125px">
+
+                Tilbage
+
+            </a>
+
+            <!-- til forsiden -->
+            <a href="index.php"
+               class="font-sixteen inter border border-dark-brown border-2 text-center text-off-black rounded-pill py-2 px-3 d-inline-block text-decoration-none"
+               style="width: 125px">
+
+                Til forsiden
+
+            </a>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!--bottom-wave-->
+<div aria-hidden="true" class="fixed-bottom d-none d-lg-block">
+    <img src="waves-desktop/404-bottom-wave.png" class="waves img-fluid  p-0 m-0" alt="">
 </div>
 
 
